c2b64e972e2f5d408a275144a19d432b3117ce52
Cheesenibbles123 <crapno2@gmail.com>
2020-11-24 02:44:40 +0000

    Added Alternion integration

--- oliverbot.js
+++ oliverbot.js
@@ -1869,13 +1869,16 @@ function sendBWStatsEmbed(message,embed){
 
 function blackwakeCommandHandler(message,args){
 	if (isAllowed){
-		switch (args[0])
+		switch (args[0].toLowerCase())
 		{
 			case "monthly":
 				fetchEloStuff(message, args[1], args[0]);
 				break;
 			case "elo":
 				fetchEloStuff(message, args[1], args[0]);
+				break;
+			case "alternion":
+				checkifInDatabase(message,args);
 				break;
 			default:
 				getBlackwakeStats(message,args);
@@ -3528,6 +3531,415 @@ function updateNWordCounter(message){
 	return;
 }
 
+// Alternion Mod integration
+
+var alternionConnectionPool = mysql.createPool({
+	connectionLimit : 30,
+	host : config.databaseInfo.host,
+	user : config.databaseInfo.user,
+	password : config.databaseInfo.password,
+	database : "Alternion"
+});
+
+function alternionHandler(message, args){
+	let alternionHandlerEmbed = new Discord.MessageEmbed();
+
+	switch (args[1].toLowerCase()){
+
+		case "listbadges":
+			alternionHandlerEmbed.setTitle("Available Badges")
+				.setFooter("The formatting is: - `badge_id` : Badge Name -");
+			getBadges(message,message.author.id,alternionHandlerEmbed);
+			break;
+
+		case "help":
+			alternionHandlerEmbed.setTitle("Help Menu")
+				.addDescription("Default usage:\n`;Blackwake` `Alternion` `Feature`\nCurrently supported features:\n- Help\n- ListBadges")
+				.setFooter("Note: Requires your discord_ID to be linked to your Steam_ID in the database, contact Archie for more information.");
+			sendAlternionEmbed(message,alternionHandlerEmbed,false);
+			break;
+
+		case "assign":
+			alternionHandlerEmbed.setTitle("Assigning Item");
+			assignItemSkin(message,args,alternionHandlerEmbed);
+			break;
+
+		case "overview":
+			getAlternionOverview(message,alternionHandlerEmbed);
+			break;
+
+		default:
+			alternionHandlerEmbed.setDescription("You have entered an incorrect command, please try again.\nUse `;Blackwake Alternion Help` to get a list of supported commands!");
+			sendAlternionEmbed(message,alternionHandlerEmbed,false);
+			break;
+
+	}
+
+	return;
+}
+
+function assignItemSkin(message,args,alternionHandlerEmbed){
+	let table1Name = "";
+	let table2Name = "";
+	let fieldName = "";
+	let table2Field = "";
+	switch (args[2].toLowerCase()){
+		case "badge":
+			table1Name = "Badge";
+			table2Name = "LimitedBadges";
+			fieldName = "Badge_ID";
+			table2Field = "Allowed_Badge_ID";
+			break;
+		case "sail":
+			table1Name = "NormalSail";
+			table2Name = "LimitedSails";
+			fieldName = "Sail_ID";
+			table2Field = "Allowed_Sail_ID";
+			break;
+		case "mainsail":
+			table1Name = "MainSail";
+			table2Name = "LimitedMainSails";
+			fieldName = "Main_sail_ID";
+			table2Field = "Allowed_Main_Sail_ID";
+			break;
+		case "goldmask":
+			table1Name = "GoldMask";
+			table2Name = "LimitedGoldMask";
+			fieldName = "Mask_ID";
+			table2Field = "Allowed_Gold_Mask_ID";
+			break;
+		case "cannon":
+			table1Name = "Cannon";
+			table2Name = "LimitedCannons";
+			fieldName = "Cannon_ID";
+			table2Field = "Allowed_Cannon_ID";
+			break;
+		case "musket":
+			table1Name = "WeaponSkin";
+			table2Name = "LimitedWeaponSkins";
+			fieldName = "Musket_ID";
+			table2Field = "Allowed_Weapon_Skin_ID";
+			break;
+		case "blunderbuss":
+			table1Name = "WeaponSkin";
+			table2Name = "LimitedWeaponSkins";
+			fieldName = "Blunderbuss_ID";
+			table2Field = "Allowed_Weapon_Skin_ID";
+			break;
+		case "nockgun":
+			table1Name = "WeaponSkin";
+			table2Name = "LimitedWeaponSkins";
+			fieldName = "Nockgun_ID";
+			table2Field = "Allowed_Weapon_Skin_ID";
+			break;
+		case "handmortar":
+			table1Name = "WeaponSkin";
+			table2Name = "LimitedWeaponSkins";
+			fieldName = "Handmortar_ID";
+			table2Field = "Allowed_Weapon_Skin_ID";
+			break;
+		case "pistol":
+			table1Name = "WeaponSkin";
+			table2Name = "LimitedWeaponSkins";
+			fieldName = "Standard_Pistol_ID";
+			table2Field = "Allowed_Weapon_Skin_ID";
+			break;
+		case "shortpistol":
+			table1Name = "WeaponSkin";
+			table2Name = "LimitedWeaponSkins";
+			fieldName = "Short_Pistol_ID";
+			table2Field = "Allowed_Weapon_Skin_ID";
+			break;
+		case "duckfoot":
+			table1Name = "WeaponSkin";
+			table2Name = "LimitedWeaponSkins";
+			fieldName = "Duckfoot_ID";
+			table2Field = "Allowed_Weapon_Skin_ID";
+			break;
+		case "matchlock":
+			table1Name = "WeaponSkin";
+			table2Name = "LimitedWeaponSkins";
+			fieldName = "Matchlock_Revolver_ID";
+			table2Field = "Allowed_Weapon_Skin_ID";
+			break;
+		case "annely":
+			table1Name = "WeaponSkin";
+			table2Name = "LimitedWeaponSkins";
+			fieldName = "Annely_Revolver_ID";
+			table2Field = "Allowed_Weapon_Skin_ID";
+			break;
+		case "axe":
+			table1Name = "WeaponSkin";
+			table2Name = "LimitedWeaponSkins";
+			fieldName = "Axe_ID";
+			table2Field = "Allowed_Weapon_Skin_ID";
+			break;
+		case "rapier":
+			table1Name = "WeaponSkin";
+			table2Name = "LimitedWeaponSkins";
+			fieldName = "Rapier_ID";
+			table2Field = "Allowed_Weapon_Skin_ID";
+			break;
+		case "dagger":
+			table1Name = "WeaponSkin";
+			table2Name = "LimitedWeaponSkins";
+			fieldName = "Dagger_ID";
+			table2Field = "Allowed_Weapon_Skin_ID";
+			break;
+		case "bottle":
+			table1Name = "WeaponSkin";
+			table2Name = "LimitedWeaponSkins";
+			fieldName = "Bottle_ID";
+			table2Field = "Allowed_Weapon_Skin_ID";
+			break;
+		case "cutlass":
+			table1Name = "WeaponSkin";
+			table2Name = "LimitedWeaponSkins";
+			fieldName = "Cutlass_ID";
+			table2Field = "Allowed_Weapon_Skin_ID";
+			break;
+		case "pike":
+			table1Name = "WeaponSkin";
+			table2Name = "LimitedWeaponSkins";
+			fieldName = "Pike_ID";
+			table2Field = "Allowed_Weapon_Skin_ID";
+			break;
+		case "tomohawk":
+			table1Name = "WeaponSkin";
+			table2Name = "LimitedWeaponSkins";
+			fieldName = "Tomohawk_ID";
+			table2Field = "Allowed_Weapon_Skin_ID";
+			break;
+		case "spyglass":
+			table1Name = "WeaponSkin";
+			table2Name = "LimitedWeaponSkins";
+			fieldName = "Spyglass_ID";
+			table2Field = "Allowed_Weapon_Skin_ID";
+			break;
+		case "grenade":
+			table1Name = "WeaponSkin";
+			table2Name = "LimitedWeaponSkins";
+			fieldName = "Grenade_ID";
+			table2Field = "Allowed_Weapon_Skin_ID";
+			break;
+		case "healitem":
+			table1Name = "WeaponSkin";
+			table2Name = "LimitedWeaponSkins";
+			fieldName = "HealItem_ID";
+			table2Field = "Allowed_Weapon_Skin_ID";
+			break;
+		case "hammer":
+			table1Name = "WeaponSkin";
+			table2Name = "LimitedWeaponSkins";
+			fieldName = "Hammer_ID";
+			table2Field = "Allowed_Weapon_Skin_ID";
+			break;
+		case "atlas":
+			table1Name = "WeaponSkin";
+			table2Name = "LimitedWeaponSkins";
+			fieldName = "atlas01_ID";
+			table2Field = "Allowed_Weapon_Skin_ID";
+			break;
+		default:
+			message.channel.send("That is not a valid item to assign!");
+			table1Name = "NA";
+			break;
+	}
+
+	if (table1Name != "NA"){
+		alternionConnectionPool.query(`SELECT ${table1Name}.Name, ${table1Name}.Display_Name, ${table1Name}.ID FROM ${table2Name} INNER JOIN User ON User_ID = User.ID INNER JOIN ${table1Name} ON ${table2Field} = ${table1Name}.ID WHERE User.Discord_ID="${message.author.id}"`, (err, rows) => {
+				alternionConnectionPool.query(`SELECT ${table1Name}.Name, ${table1Name}.Display_Name, ${table1Name}.ID FROM ${table1Name} WHERE Limited!=True`, (err, rows2) => {
+					console.log(rows);
+					console.log(rows2);
+					let found = false;
+					let assignedBadge = "";
+					if (rows){
+						for (let i = 0; i < rows.length; i++){
+							if (args[3] === rows[i].Name){
+								alternionConnectionPool.query(`UPDATE User SET ${fieldName}=${rows[i].ID} WHERE Discord_ID="${message.author.id}"`);
+								console.log(`Found match: -${args[3]}- === -${rows[i].Name}-`);
+								assignedBadge = rows[i].Display_Name;
+								found = true;
+								break;
+							}
+						}
+					}
+
+					if (rows2){
+						for (let i = 0; i < rows2.length; i++){
+							if (args[3] === rows2[i].Name){
+								alternionConnectionPool.query(`UPDATE User SET ${fieldName}=${rows2[i].ID} WHERE Discord_ID="${message.author.id}"`);
+								console.log(`Found match: -${args[3]}- === -${rows2[i].Name}-`);
+								assignedBadge = rows2[i].Display_Name;
+								found = true;
+								break;
+							}
+						}
+					}
+
+					if (!found){
+						alternionHandlerEmbed.setDescription("You cannot assign that Item!");
+					}else{
+						alternionHandlerEmbed.setDescription(`Assigned badge: **${assignedBadge}**`);
+					}
+
+					sendAlternionEmbed(message,alternionHandlerEmbed,true);
+
+				});
+			});
+	}
+}
+
+function getAlternionOverview(message,alternionHandlerEmbed){
+
+	// Prepare for an SQL nightmare
+
+	alternionConnectionPool.query(`SELECT Badge.Display_Name AS bad, GoldMask.Display_Name AS mas, NormalSail.Display_Name AS sai, MainSail.Display_Name AS msa, Cannon.Display_Name AS can, Musket.Display_Name AS mus, Blunderbuss.Display_Name AS blu, Nockgun.Display_Name AS noc, HM.Display_Name AS han, Pis.Display_Name AS pis, Spis.Display_Name AS spi, Duck.Display_Name AS duc, Mat.Display_Name AS mat, Ann.Display_Name AS ann, Axe.Display_Name AS axe, Rap.Display_Name AS rap, Dag.Display_Name AS dag, Bot.Display_Name AS bot, Cut.Display_Name AS cut, Pik.Display_Name AS pik, Tom.Display_Name AS tom, Spy.Display_Name AS spy, Gre.Display_Name AS gre, Hea.Display_Name AS hea, Ham.Display_Name AS ham, Atl.Display_Name AS atl FROM User INNER JOIN Badge ON Badge_ID = Badge.ID INNER JOIN GoldMask ON Mask_ID = GoldMask.ID INNER JOIN NormalSail ON Sail_ID = NormalSail.ID INNER JOIN MainSail ON Main_Sail_ID = MainSail.ID INNER JOIN Cannon ON Cannon_ID = Cannon.ID INNER JOIN WeaponSkin AS Musket ON Musket_ID = Musket.ID INNER JOIN WeaponSkin AS Blunderbuss ON Blunderbuss_ID = Blunderbuss.ID INNER JOIN WeaponSkin AS Nockgun ON Nockgun_ID = Nockgun.ID INNER JOIN WeaponSkin AS HM ON Handmortar_ID = HM.ID INNER JOIN WeaponSkin AS Pis ON Standard_Pistol_ID = Pis.ID INNER JOIN WeaponSkin AS Spis ON Short_Pistol_ID = Spis.ID INNER JOIN WeaponSkin AS Duck ON Duckfoot_ID = Duck.ID INNER JOIN WeaponSkin AS Mat ON Matchlock_Revolver_ID = Mat.ID INNER JOIN WeaponSkin AS Ann ON Annely_Revolver_ID = Ann.ID INNER JOIN WeaponSkin AS Axe ON Axe_ID = Axe.ID INNER JOIN WeaponSkin AS Rap ON Rapier_ID = Rap.ID INNER JOIN WeaponSkin AS Dag ON Dagger_ID = Dag.ID INNER JOIN WeaponSkin AS Bot ON Bottle_ID = Bot.ID INNER JOIN WeaponSkin AS Cut ON Cutlass_ID = Cut.ID INNER JOIN WeaponSkin AS Pik ON Pike_ID = Pik.ID INNER JOIN WeaponSkin AS Tom ON Tomohawk_ID = Tom.ID INNER JOIN WeaponSkin AS Spy ON Spyglass_ID = Spy.ID INNER JOIN WeaponSkin AS Gre ON Grenade_ID = Gre.ID INNER JOIN WeaponSkin AS Hea ON HealItem_ID = Hea.ID INNER JOIN WeaponSkin AS Ham ON Hammer_ID = Ham.ID INNER JOIN WeaponSkin AS Atl ON atlas01_ID = Atl.ID WHERE Discord_ID="${message.author.id}"`, (err,rows) => {
+		if (rows.length === 0){
+			message.channel.send("You are currently not in the database, please contact Archie.");
+			return;
+		} else if (rows[1]){
+			message.channel.send("You have multiple entries, please contact Archie.");
+			return;
+		}else{
+			let finalMsg = `Badge       : ${rows[0].bad}\n`
+						+	`Gold Mask   : ${rows[0].mas}\n`
+						+	`Sail        : ${rows[0].sai}\n`
+						+	`Mail Sail   : ${rows[0].msa}\n`
+						+	`Cannon      : ${rows[0].can}\n`
+						+	`Musket      : ${rows[0].mus}\n`
+						+	`Blunderbuss : ${rows[0].blu}\n`
+						+	`Nockgun     : ${rows[0].noc}\n`
+						+	`HandMortar  : ${rows[0].han}\n`
+						+	`Pistol      : ${rows[0].pis}\n`
+						+	`Short pistol: ${rows[0].spi}\n`
+						+	`Duckfoot    : ${rows[0].duc}\n`
+						+	`Matchlock   : ${rows[0].mat}\n`
+						+	`Annely      : ${rows[0].ann}\n`
+						+	`Axe         : ${rows[0].axe}\n`
+						+	`Rapier      : ${rows[0].rap}\n`
+						+	`Dagger      : ${rows[0].dag}\n`
+						+	`Bottle      : ${rows[0].bot}\n`
+						+	`Cutlass     : ${rows[0].cut}\n`
+						+	`Pike        : ${rows[0].pik}\n`
+						+	`Tomohawk    : ${rows[0].tom}\n`
+						+	`Spyglass    : ${rows[0].spy}\n`
+						+	`Grenade     : ${rows[0].gre}\n`
+						+	`Healing     : ${rows[0].hea}\n`
+						+	`Hammer      : ${rows[0].ham}\n`
+						+	`Atlas01     : ${rows[0].atl}\n`;
+			alternionHandlerEmbed.setDescription(finalMsg);
+			sendAlternionEmbed(message,alternionHandlerEmbed,false);
+		}
+	});
+}
+
+function checkifInDatabase(message,args){
+	alternionConnectionPool.query(`SELECT * FROM User WHERE Discord_ID="${message.author.id}"`, (err,rows) => {
+		if (rows.length > 1){
+			message.channel.send("You appear to have two accounts linked to this discord account, please contact Archie.");
+			return;
+		}else if (rows.length == 0){
+			message.channel.send("Your discord account is not linked to your steamID, please contact Archie.");
+			return;
+		}else{
+			alternionHandler(message,args);
+		}
+	});
+}
+
+function getBadges(message,ID,alternionHandlerEmbed){
+	alternionConnectionPool.query(`SELECT Badge.Name, Badge.Display_Name FROM LimitedBadges INNER JOIN User ON User_ID = User.ID INNER JOIN Badge ON Allowed_badge_ID = Badge.ID WHERE User.Discord_ID="${ID}"`, (err,rows) => {
+		if (rows.length < 1){
+			alternionHandlerEmbed.setDescription("No badges found.");
+		}else{
+			let returnString = "";
+			for (let i = 0; i < rows.length; i++){
+				returnString += `\`${rows[i].Name}\` : ${rows[i].Display_Name}\n`;
+			}
+			alternionHandlerEmbed.setDescription(returnString);
+		}
+
+		sendAlternionEmbed(message,alternionHandlerEmbed,false);
+	});
+}
+
+function sendAlternionEmbed(message,embed,needsUpdate){
+	message.channel.send(embed);
+	if (needsUpdate){
+		getLocalJson(message.author.id);
+	}
+	return;
+}
+
+var alternionJsonFile = null;
+
+function getLocalJson(discord_id){
+	let filepath = config.alternion.jsonFilePath;
+	if (alternionJsonFile === null){
+		fs.readFile(filepath, function (err,data) {
+			json = '{ "users" : [' + data.toString().replace(/&/g, ",") + ']}';
+			alternionJsonFile = JSON.parse(`${json}`);
+			updateLocalCache(discord_id,filepath);
+		});
+	}else{
+		updateLocalCache(discord_id,filepath);
+	}
+	return;
+}
+
+function updateLocalCache(discord_id,filepath){
+	alternionConnectionPool.query(`SELECT User.Steam_ID AS steam_ID, Badge.Name AS bad, GoldMask.Name AS mas, NormalSail.Name AS sai, MainSail.Name AS msa, Cannon.Name AS can, Musket.Name AS mus, Blunderbuss.Name AS blu, Nockgun.Name AS noc, HM.Name AS han, Pis.Name AS pis, Spis.Name AS spi, Duck.Name AS duc, Mat.Name AS mat, Ann.Name AS ann, Axe.Name AS axe, Rap.Name AS rap, Dag.Name AS dag, Bot.Name AS bot, Cut.Name AS cut, Pik.Name AS pik, Tom.Name AS tom, Spy.Name AS spy, Gre.Name AS gre, Hea.Name AS hea, Ham.Name AS ham, Atl.Name AS atl FROM User INNER JOIN Badge ON Badge_ID = Badge.ID INNER JOIN GoldMask ON Mask_ID = GoldMask.ID INNER JOIN NormalSail ON Sail_ID = NormalSail.ID INNER JOIN MainSail ON Main_Sail_ID = MainSail.ID INNER JOIN Cannon ON Cannon_ID = Cannon.ID INNER JOIN WeaponSkin AS Musket ON Musket_ID = Musket.ID INNER JOIN WeaponSkin AS Blunderbuss ON Blunderbuss_ID = Blunderbuss.ID INNER JOIN WeaponSkin AS Nockgun ON Nockgun_ID = Nockgun.ID INNER JOIN WeaponSkin AS HM ON Handmortar_ID = HM.ID INNER JOIN WeaponSkin AS Pis ON Standard_Pistol_ID = Pis.ID INNER JOIN WeaponSkin AS Spis ON Short_Pistol_ID = Spis.ID INNER JOIN WeaponSkin AS Duck ON Duckfoot_ID = Duck.ID INNER JOIN WeaponSkin AS Mat ON Matchlock_Revolver_ID = Mat.ID INNER JOIN WeaponSkin AS Ann ON Annely_Revolver_ID = Ann.ID INNER JOIN WeaponSkin AS Axe ON Axe_ID = Axe.ID INNER JOIN WeaponSkin AS Rap ON Rapier_ID = Rap.ID INNER JOIN WeaponSkin AS Dag ON Dagger_ID = Dag.ID INNER JOIN WeaponSkin AS Bot ON Bottle_ID = Bot.ID INNER JOIN WeaponSkin AS Cut ON Cutlass_ID = Cut.ID INNER JOIN WeaponSkin AS Pik ON Pike_ID = Pik.ID INNER JOIN WeaponSkin AS Tom ON Tomohawk_ID = Tom.ID INNER JOIN WeaponSkin AS Spy ON Spyglass_ID = Spy.ID INNER JOIN WeaponSkin AS Gre ON Grenade_ID = Gre.ID INNER JOIN WeaponSkin AS Hea ON HealItem_ID = Hea.ID INNER JOIN WeaponSkin AS Ham ON Hammer_ID = Ham.ID INNER JOIN WeaponSkin AS Atl ON atlas01_ID = Atl.ID WHERE Discord_ID="${discord_id}"`, (err,rows) => {
+		for (let i = 0; i < alternionJsonFile.users.length; i++){
+			if (alternionJsonFile.users[i].steamID === rows[0].steam_ID){
+				console.log(`Found User: -${alternionJsonFile.users[i].steamID}- => -${discord_id}-`);
+				alternionJsonFile.users[i].badgeName = rows[0].bad;
+				alternionJsonFile.users[i].maskSkinName = rows[0].mas;
+				alternionJsonFile.users[i].sailSkinName = rows[0].sai;
+				alternionJsonFile.users[i].mainSailName = rows[0].msa;
+				alternionJsonFile.users[i].cannonSkinName = rows[0].can;
+				alternionJsonFile.users[i].musketSkinName = rows[0].mus;
+				alternionJsonFile.users[i].blunderbussSkinName = rows[0].blu;
+				alternionJsonFile.users[i].nockgunSkinName = rows[0].noc;
+				alternionJsonFile.users[i].handMortarSkinName = rows[0].han;
+				alternionJsonFile.users[i].standardPistolSkinName = rows[0].pis;
+				alternionJsonFile.users[i].shortPistolSkinName = rows[0].spi;
+				alternionJsonFile.users[i].duckfootSkinName = rows[0].duc;
+				alternionJsonFile.users[i].matchlockRevolverSkinName = rows[0].mat;
+				alternionJsonFile.users[i].annelyRevolverSkinName = rows[0].ann;
+				alternionJsonFile.users[i].axeSkinName = rows[0].axe;
+				alternionJsonFile.users[i].rapierSkinName = rows[0].rap;
+				alternionJsonFile.users[i].daggerSkinName = rows[0].dag;
+				alternionJsonFile.users[i].bottleSkinName = rows[0].bot;
+				alternionJsonFile.users[i].cutlassSkinName = rows[0].cut;
+				alternionJsonFile.users[i].pikeSkinName = rows[0].pik;
+				alternionJsonFile.users[i].tomohawkSkinName = rows[0].tom;
+				alternionJsonFile.users[i].spyglassSkinName = rows[0].spy;
+				alternionJsonFile.users[i].grenadeSkinName = rows[0].gre;
+				alternionJsonFile.users[i].healItemSkinName = rows[0].hea;
+				alternionJsonFile.users[i].hammerSkinName = rows[0].ham;
+				alternionJsonFile.users[i].atlas01SkinName = rows[0].atl;
+				break;
+			}
+		}
+
+		updateJsonFile(filepath);
+	});
+	return;
+}
+
+function updateJsonFile(filepath){
+	let writeString = "";
+	for (let i = 0; i < alternionJsonFile.users.length; i++){
+		if (i < alternionJsonFile.users.length - 1){
+			writeString += JSON.stringify(alternionJsonFile.users[i]).toString() + "&";
+		}else{
+			writeString += JSON.stringify(alternionJsonFile.users[i]).toString();
+		}
+	}
+	fs.writeFile(filepath, writeString.toString(), function(err){});
+}
+
 allowChannels = ["512331083493277706","577180597521350656","440525025452490752","663524428092538920","563478316120539147"];
 
 bot.on("ready", () => {
@@ -5104,7 +5516,7 @@ bot.on('raw', async event => {
 			break;
 		case "MESSAGE_REACTION_ADD":
 			if (event.d.channel_id === "762401591180525608"){
-				manageJoinReaction(event);
+				// manageJoinReaction(event);
 			}else if (parseInt(event.d.channel_id) !== 607491352598675457 && adjustableConfig.reactions.reactionMenu){
         		break;
     		}
