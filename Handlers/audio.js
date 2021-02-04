85331c462c47c9dc2dfb4e7e63d2bb32f0383e64
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-02-04 02:53:53 +0000

    Added playAudio function

--- Handlers/audio.js
+++ Handlers/audio.js
@@ -65,3 +65,23 @@ function stopAudio(message){
 		message.channel.send("I have stopped");
 	}
 }
+
+async function playAudio(message,args,voiceChannel){
+	isPlaying = true;
+	voiceChannel.join().then(connection =>{
+		currentDispatcher = connection
+			.play(
+        	  	ytdl(args.join(""),{filter:'audioonly',quality:'highestaudio',highWaterMark:1<<25}, {highWaterMark: 1},{bitrate: 192000})
+      		)
+      		.on("finish",() =>{
+      			voiceChannel.leave();
+      			isPlaying = false;
+      		})
+      		.on("error",e=>{
+      			console.error(e);
+     		 	voiceChannel.leave();
+     		 	isPlaying = false;
+     		});
+     	currentDispatcher.setVolumeLogarithmic(1);
+    });
+}
