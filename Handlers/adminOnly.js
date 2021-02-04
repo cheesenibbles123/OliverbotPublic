2619c6a1e9e5b4cfaa1eda80c5d001295e19cd77
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-02-04 03:20:59 +0000

    Added ban command to admin handler

--- Handlers/adminOnly.js
+++ Handlers/adminOnly.js
@@ -31,6 +31,9 @@ exports.handler = function handler(message,command,args){
 			case "channelinf":
 				getChannelInformation(message);
 				break;
+			case "ban":
+				ban(message);
+				break;
 		}
 	}else{
 		message.channel.send(issueEmbed.grabEmbed(0,null));
@@ -277,4 +280,23 @@ function getChannelInformation(message){
 			.setTimestamp();
 		message.channel.send(channelinfo);
 	});
+}
+
+function ban(message){
+	if (typeof args[0] === "string"){
+		try{
+			let member = message.guild.members.find('id',message.mentions.users.first().id);
+			if (member.roles.has(config.serverInfo.roles.serverAdministrator)){
+				message.channel.send("You can't ban an admin!");
+			}else{
+				message.guild.members.ban(member);
+				bot.channels.cache.get(config.serverInfo.channels.loggingChannel).send("User: "+member+", has been banned. Reason: "+(args.slice(2)).join(" ")+"\n"
+															+"Banned by: "+message.member.user.username+".");
+			}
+		}catch(e){
+			message.channel.send("Please enter a correct member.");
+		}
+	}else{
+		message.channel.send("Please enter a User.");
+	}
 }
--- Handlers/mainCommandHandler.js
+++ Handlers/mainCommandHandler.js
@@ -105,6 +105,7 @@ exports.handler = function handler(message,command,args){
 		case "botinfo":
 		case "serverinfo":
 		case "channelinfo":
+		case "ban":
 			adminCommands.handler(message,command,args);
 			break;
         case "random":
@@ -150,28 +151,6 @@ exports.handler = function handler(message,command,args){
 				}else{
 					message.channel.send("You cannot use this command");
 				}	
-			}else{
-				lackingPermissions(message);
-			}
-			break;
-		case "ban":
-			if (message.member.roles.cache.has(config.serverInfo.roles.serverAdministrator)){
-				if (typeof args[0] === "string"){
-					try{
-						let member = message.guild.members.find('id',message.mentions.users.first().id);
-						if (member.roles.has(config.serverInfo.roles.serverAdministrator)){
-							message.channel.send("You can't ban an admin!");
-						}else{
-							message.guild.members.ban(member);
-							bot.channels.cache.get(config.serverInfo.channels.loggingChannel).send("User: "+member+", has been banned. Reason: "+(args.slice(2)).join(" ")+"\n"
-																		+"Banned by: "+message.member.user.username+".");
-						}
-					}catch(e){
-						message.channel.send("Please enter a correct member.");
-					}
-				}else{
-					message.channel.send("Please enter a User.");
-				}
 			}else{
 				lackingPermissions(message);
 			}
