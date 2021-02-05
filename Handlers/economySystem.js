97e9e7d405126d03ebff88ead834a57da0119bdc
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-02-05 15:21:37 +0000

    Added shopwindow

--- Handlers/economySystem.js
+++ Handlers/economySystem.js
@@ -14,6 +14,10 @@ var leaderboardlimits = {
 	"usernameEco" : 20
 }
 
+var delays = {
+	xpLb = 300000;
+}
+
 exports.handler = function handler(message,command,args){
 	switch (command){
 		case "beg":
@@ -32,77 +36,78 @@ exports.handler = function handler(message,command,args){
 			sellItem(message.author.id,args[0],message);
 			break;
 		case "giftcoins":
+			let user = glob.getUserFromMention(args[0]);
 			giftUserCoins(message.author.id,user.id,args[1],message);
 			break;
 		case "gamble":
-			gambleMoney(args[0],message);
+			gambleMoney(args[0],message,args);
 			break;
 	}
 }
 
 exports.initEconomy = function initEconomy(){
-	updateleaderboard();
 	bot = require("./../oliverbot.js").bot;
+	setInterval(function(){
+		updateleaderboard();
+	},delays.xpLb);
+	updateShopWindow();
 }
 
 async function updateleaderboard(){
-	let delay = 300000;
-	setInterval(function(){
-		db.mainDatabaseConnectionPool.query(`SELECT * FROM xp order by level desc, xp desc limit 31`, (err,rows,fields) =>{
-			let length = 0;
-			if (rows.length<leaderboardlimits.listsizelimit){
-				length = rows.length;
+	db.mainDatabaseConnectionPool.query(`SELECT * FROM xp order by level desc, xp desc limit 31`, (err,rows,fields) =>{
+		let length = 0;
+		if (rows.length<leaderboardlimits.listsizelimit){
+			length = rows.length;
+		}else{
+			length = leaderboardlimits.listsizelimit; 
+		}
+		let finalmsg = "```diff\n"
+						+"-XP LeaderBoard\n"
+						+"+Rank  Username              level     XP\n";
+		for (i=0;i<length;){
+			let rank = (i+1).toString();
+			if (rank.length > leaderboardlimits.rank){
+				console.log("EXPAND SIZE");
 			}else{
-				length = leaderboardlimits.listsizelimit; 
+				let x = leaderboardlimits.rank - rank.length;
+				rank = rank + new Array(x + 1).join(' ');
 			}
-			let finalmsg = "```diff\n"
-							+"-XP LeaderBoard\n"
-							+"+Rank  Username              level     XP\n";
-			for (i=0;i<length;){
-				let rank = (i+1).toString();
-				if (rank.length > leaderboardlimits.rank){
-					console.log("EXPAND SIZE");
-				}else{
-					let x = leaderboardlimits.rank - rank.length;
-					rank = rank + new Array(x + 1).join(' ');
-				}
-				var user;
-				let username = "";
-				try{
-					user = bot.users.cache.get(rows[i].id);
-					username = user.username;
-				}catch(e){
-					username = rows[i].id;
-					//console.log(user);
-					//console.log(e);
-				}
-				if (username.length > leaderboardlimits.username){
-					username = username.slice(0,leaderboardlimits.username);
-				}else{
-					let x = leaderboardlimits.username - username.length;
-					username = username + new Array(x + 1).join(' ');
-				}
-				let level = rows[i].level.toString();
-				if (level.length > leaderboardlimits.level){
-					level.slice(0,leaderboardlimits.level);
-				}else{
-					let x = leaderboardlimits.level - level.length;
-					level = level + new Array(x + 1).join(' ');
-				}
-				let xp = rows[i].xp.toString();
-				if (xp.length > leaderboardlimits.xp){
-					score.slice(0,leaderboardlimits.xp);
-				}else{
-					let x = leaderboardlimits.xp - xp.length;
-					xp = xp + new Array(x + 1).join(' ');
-				}
-				finalmsg = finalmsg + `${rank} | ${username} | ${level} | ${xp}\n`;
-				i++;
+			var user;
+			let username = "";
+			try{
+				user = bot.users.cache.get(rows[i].id);
+				username = user.username;
+			}catch(e){
+				username = rows[i].id;
+				//console.log(user);
+				//console.log(e);
 			}
-			finalmsg = finalmsg+"```";
-			bot.channels.cache.get(config.serverInfo.channels.xpLeaderboard).messages.fetch(config.serverInfo.messages.xpLeaderboard).then(msg => {msg.edit(finalmsg);});
-		});
-	},delay);
+			if (username.length > leaderboardlimits.username){
+				username = username.slice(0,leaderboardlimits.username);
+			}else{
+				let x = leaderboardlimits.username - username.length;
+				username = username + new Array(x + 1).join(' ');
+			}
+			let level = rows[i].level.toString();
+			if (level.length > leaderboardlimits.level){
+				level.slice(0,leaderboardlimits.level);
+			}else{
+				let x = leaderboardlimits.level - level.length;
+				level = level + new Array(x + 1).join(' ');
+			}
+			let xp = rows[i].xp.toString();
+			if (xp.length > leaderboardlimits.xp){
+				score.slice(0,leaderboardlimits.xp);
+			}else{
+				let x = leaderboardlimits.xp - xp.length;
+				xp = xp + new Array(x + 1).join(' ');
+			}
+			finalmsg = finalmsg + `${rank} | ${username} | ${level} | ${xp}\n`;
+			i++;
+		}
+		finalmsg = finalmsg+"```";
+		bot.channels.cache.get(config.serverInfo.channels.xpLeaderboard).messages.fetch(config.serverInfo.messages.xpLeaderboard).then(msg => {msg.edit(finalmsg);});
+	});
 }
 
 exports.levelsystem = function levelsystem(xp,currentlevel){
@@ -335,7 +340,7 @@ function purchaseItem(ID,item,message,args){
 									.setDescription(desc);
 								bot.channels.cache.get("718232760388550667").send(logEmbed);
 							}
-							// updateShopWindow();
+							updateShopWindow();
 							// displayRichestUsers();
 						}
 					});	
@@ -485,7 +490,7 @@ function sellItem(ID,item,message){
 						let sellEmbed = new Discord.MessageEmbed().setTitle("Item Sold").setDescription(`Item: ${itemInfo.name} has been sold for ${worth}.\nSold by: ${message.author}`).setTimeStamp();
 						message.channel.send(sellEmbed);
 						bot.channels.cache.get(config.serverInfo.channels.economy.bigTransactionLoggingChannel).send(sellEmbed);
-						// updateShopWindow();
+						updateShopWindow();
 						// displayRichestUsers();
 					});
 				}
@@ -556,7 +561,7 @@ function giftUserCoins(gifterID,recieverID,amount,message){
 	}
 }
 
-function gambleMoney(amount,message){
+function gambleMoney(amount,message,args){
 	if (args[0] && (args.length === 1)){
 		db.mainDatabaseConnectionPool.query(`SELECT * FROM inventoryGT WHERE ID='${message.author.id}'`, (err,rows) =>{
 			if (isNaN(amount)){
@@ -566,7 +571,7 @@ function gambleMoney(amount,message){
 			}else if (amount < 5){
 				message.channel.send("You must gamble a minimum of 5 coins!");
 			}else{
-				let result = getRandomInt(30);
+				let result = glob.getRandomInt(30);
 				switch(true){
 					case result < 20:
 						income = 0;
@@ -606,4 +611,199 @@ function gambleMoney(amount,message){
 	}else{
 		message.channel.send("Please use the correct format! `;gamble` `amount`");
 	}
+}
+
+function updateShopWindow(){
+	db.mainDatabaseConnectionPool.query(`SELECT * FROM shop`, (err,rows) => {
+		let newShopListing = new Discord.MessageEmbed().setTitle("Shop Channel");
+
+		let melee = "";
+		let secondary = "";
+		let primary = "";
+
+		let smallShips = "";
+		let bigShips = "";
+		let megaShips = "";
+
+		let swivelTypes = "";
+		let cannons = "";
+
+		let incomeSmall = "";
+		let incomeMed = "";
+		let incomeLarge = "";
+
+		let birds = "";
+		let Reptiles = "";
+		let Mammals = "";
+
+		let basicMaterials = "";
+		let advancedMaterials = "";
+		for (i=0;i<rows.length;i++){
+			let itemInfo = JSON.parse(rows[i].info);
+			switch(itemInfo.type){
+				case "megaShips":
+					megaShips = megaShips + `${itemInfo.name} - ${itemInfo.crew}crew\n` + " - `" + `${rows[i].name}` + "`\n";
+					break;
+				case "smallShips":
+					smallShips = smallShips + `${itemInfo.name} - ${itemInfo.crew}crew\n` + " - `" + `${rows[i].name}` + "`\n";
+					break;
+				case "largeShips":
+					bigShips = bigShips + `${itemInfo.name} - ${itemInfo.crew}crew\n` + " - `" + `${rows[i].name}` + "`\n";
+					break;
+				case "primary":
+					primary = primary + `${itemInfo.name}` + " - `" + `${rows[i].name}` + "`\n";
+					break;
+				case "secondary":
+					secondary = secondary + `${itemInfo.name}` + " - `" + `${rows[i].name}` + "`\n";
+					break;
+				case "melee":
+					melee = melee + `${itemInfo.name}` + " - `" + `${rows[i].name}` + "`\n";
+					break;
+				case "swivelType":
+					swivelTypes = swivelTypes + `${itemInfo.name}` + " - `" + `${rows[i].name}` + "`\n";
+					break;
+				case "cannons":
+					cannons = cannons + `${itemInfo.name}` + " - `" + `${rows[i].name}` + "`\n";
+					break;
+				case "income":
+					switch(itemInfo.size)
+					{
+						case "Small":
+							incomeSmall += `${itemInfo.name}` + " - `" + `${rows[i].name}` + "`\n"
+							break;
+						case "Medium":
+							incomeMed += `${itemInfo.name}` + " - `" + `${rows[i].name}` + "`\n"
+							break;
+						case "Large":
+							incomeLarge += `${itemInfo.name}` + " - `" + `${rows[i].name}` + "`\n"
+							break;
+						default:
+							break;
+					}
+					break;
+				case "pet":
+					switch(itemInfo.petType)
+					{
+						case "Bird":
+							birds += `${itemInfo.name}` + " - `" + `${rows[i].name}` + "`\n";
+							break;
+						case "Reptile":
+							Reptiles += `${itemInfo.name}` + " - `" + `${rows[i].name}` + "`\n";
+							break;
+						case "Mammal":
+							Mammals += `${itemInfo.name}` + " - `" + `${rows[i].name}` + "`\n";
+							break;
+						default:
+							break;
+					}
+					break;
+				case "constructionResources":
+					switch (itemInfo.materialType)
+					{
+						case "basicMaterial":
+							basicMaterials +=  `${itemInfo.name}` + " - `" + `${rows[i].name}` + "`\n";
+							break;
+						case "advancedMaterial":
+							advancedMaterials +=  `${itemInfo.name}` + " - `" + `${rows[i].name}` + "`\n";
+							break;
+						default:
+							break;
+					}
+					break;
+				default:
+					break;
+			}
+		}
+			db.configurationDatabaseConnectionPool.query(`SELECT * FROM economyInformation`, (err, rows2) => {
+
+				let shopWindowChannel = "";
+
+				let shopInformationMessage = "";
+				let shopIncomeMessage = "";
+				let shopWeaponsMessage = "";
+				let shopShipsMessage = "";
+				let shopPetsMessage = "";
+				let shopConstructionMessage = "";
+				let shopShipsArnamentMessage = "";
+
+				for (let s = 0; s < rows2.length; s++)
+				{
+					switch (rows2[s].name)
+					{
+						case "shopWindowChannel":
+							shopWindowChannel = rows2[s].channelID;
+							break;
+						case "shopInformationMessage":
+							shopInformationMessage = rows2[s].messageID;
+						case "shopIncomeMessage":
+							shopIncomeMessage = rows2[s].messageID;
+							break;
+						case "shopWeaponsMessage":
+							shopWeaponsMessage = rows2[s].messageID;
+							break;
+						case "shopShipsMessage":
+							shopShipsMessage = rows2[s].messageID;
+							break;
+						case "shopPetsMessage":
+							shopPetsMessage = rows2[s].messageID;
+							break;
+						case "shopShipsArnamentMessage":
+							shopShipsArnamentMessage = rows2[s].messageID;
+							break;
+						case "shopConstructionMessage":
+							shopConstructionMessage = rows2[s].messageID;
+							break;
+						default:
+							break;
+					}
+				}
+				let shopInfoEmbed = new Discord.MessageEmbed()
+					.setTitle("Shop Window")
+					.setDescription("Items that can be purchased are listed within the containers below in the following format:\n> Item Name - `ITEMID`\nNote: you do not need to make it a `code block`, you can simply type out the itemID")
+					.addField('Search', "To search for an item you search based off the `ITEMID` of the item you wish to search, for example:\n;search long9pounder",true)
+					.addField('Purchase', "To purchase an item, you purchase based off the `ITEMID` of the item you wish to purchase, for example:\n ;purchase jeanbart",true);
+				editMsg(shopInfoEmbed, shopWindowChannel, shopInformationMessage);
+
+				let shopIncomeEmbed = new Discord.MessageEmbed()
+					.setTitle("Income")
+					.addField('Small', `${incomeSmall}`,true)
+					.addField('Medium', `${incomeMed}`,true)
+					.addField('Large', `${incomeLarge}`,true);
+				editMsg(shopIncomeEmbed, shopWindowChannel, shopIncomeMessage);
+
+				let shopWeaponsEmbed = new Discord.MessageEmbed()
+					.setTitle("Weapons")
+					.addField('Primary', `${primary}`,true)
+					.addField('Secondary', `${secondary}`,true)
+					.addField('Melee', `${melee}`,true);
+				editMsg(shopWeaponsEmbed, shopWindowChannel, shopWeaponsMessage);
+
+				let shopShipsEmbed = new Discord.MessageEmbed()
+					.setTitle("Ships")
+					.addField('Small', `${smallShips}`,true)
+					.addField('Big', `${bigShips}`,true)
+					.addField('Mega', `${megaShips}`,true);
+				editMsg(shopShipsEmbed, shopWindowChannel, shopShipsMessage);
+
+				let shopShipsArnamentEmbed = new Discord.MessageEmbed()
+					.setTitle("Ship Arnament")
+					.addField('Swivels', `${swivelTypes}`,true)
+					.addField('Cannons', `${cannons}`,true);
+				editMsg(shopShipsArnamentEmbed, shopWindowChannel, shopShipsArnamentMessage);
+
+				let shopPetsEmbed = new Discord.MessageEmbed()
+					.setTitle("Pets")
+					.addField('Birds', `${birds}`,true)
+					.addField('Mammals', `${Mammals}`,true)
+					.addField('Reptiles', `${Reptiles}`,true);
+				editMsg(shopPetsEmbed, shopWindowChannel, shopPetsMessage);
+
+				let shopConstructionEmbed = new Discord.MessageEmbed()
+					.setTitle("Construction Materials")
+					.addField('Basic', `${basicMaterials}`,true)
+					.addField('Advanced', `${advancedMaterials}`,true);
+				editMsg(shopConstructionEmbed, shopWindowChannel, shopConstructionMessage);
+
+			});
+	});
 }
