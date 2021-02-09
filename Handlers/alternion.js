48391cb32282d55fb0deec1a20b1a56353b4bbc1
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-02-09 14:23:11 +0000

    Added default steam_ID showcase if discord_ID is null

--- Handlers/alternion.js
+++ Handlers/alternion.js
@@ -727,10 +727,14 @@ function teamLeaderUpdateUser(message,team,tlTeam,userID,action,alternionHandler
 function teamLeaderFetchList(message,tlID,alternionHandlerEmbed){
 	db.alternionConnectionPool.query(`SELECT Team_Leader, Team_ID FROM User WHERE discord_id=${tlID}`, (err,rows) => {
 		if (rows.length === 1 && rows[0].Team_Leader !== 0){
-			db.alternionConnectionPool.query(`SELECT ID, Discord_ID FROM User WHERE Team_ID=${rows[0].Team_ID}`, (err,rows2) => {
+			db.alternionConnectionPool.query(`SELECT ID, Steam_ID, Discord_ID FROM User WHERE Team_ID=${rows[0].Team_ID}`, (err,rows2) => {
 				let list = "";
 				for (let i=0; i < rows2.length; i++){
-					list += `\`${rows2[i].ID}\` <@${rows2[i].Discord_ID}>\n`;
+					if (rows2[i].Discord_ID != 'NULL'){
+						list += `\`${rows2[i].ID}\` : <@${rows2[i].Discord_ID}>\n`;
+					}else{
+						list += `\`${rows2[i].ID}\` : \`${rows2[i].Steam_ID}\`\n`;
+					}
 				}
 				db.alternionConnectionPool.query(`SELECT Name FROM team WHERE ID=${rows[0].Team_ID}`, (err,rows3) => {
 					alternionHandlerEmbed.setTitle(rows3[0].Name)
