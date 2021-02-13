a64adb095acb687cfa8d2b2ba172806362e4901b
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-02-13 14:52:23 +0000

    Added more issue embeds to handler

--- Handlers/issueEmbed.js
+++ Handlers/issueEmbed.js
@@ -1,58 +1,68 @@
 const Discord = require("discord.js");
 
-exports.grabEmbed = function grabEmbed(type,additionalInfo){
+exports.grabEmbed = function grabEmbed(message,type,additionalInfo){
+	let embed;
 	switch(type){
 		case 0:
-			return lackingPermissions();
+			embed.setDescription("🛑 You do not have the right permissions to use this! 🛑");
+			break;
 		case 1:
-			return apiTimeout();
+			embed.setDescription("Due to steamAPI limitations this command has a small cooldown. Please try again shortly.");
+			break;
 		case 2:
-			return lackingLevel(additionalInfo);
+			embed = lackingLevel(embed,additionalInfo);
+			break;
 		case 3:
-			return generalError(additionalInfo);
+			embed = generalError(embed,additionalInfo);
+			break;
 		case 4:
-			return disabled();
+			embed.setDescription("This command is currently disabled.");
+			break;
 		case 5:
-			return somethingWentWrong(additionalInfo);
+			embed = somethingWentWrong(embed,additionalInfo);
+			break;
+		case 6:
+			embed.setDescription("Undefined rows.");
+			break;
+		case 7:
+			embed = incorrectRowLength(embed,additionalInfo);
+			break;
+		case 8:
+			embed = incorrectRowLength(embed,additionalInfo);
+			break;
+		case 9:
+			return "9 is a success code!";
 		default:
-			return null;
+			embed.setDescription(`Unknown error at location\`${additionalInfo}\``);
+			break;
 	}
+
+	message.channel.send(embed);
 }
 
-function lackingPermissions(){
-	let embed = new Discord.MessageEmbed()
-		.setDescription("🛑 You do not have the right permissions to use this! 🛑");
+function incorrectRowLength(embed, info){
+	embed.setDescription(`Incorrect row length: ${info.length}.\nHandler: ${info.location}`);
 	return embed;
 }
 
-function apiTimeout(){
-	let embed = new Discord.MessageEmbed()
-		.setDescription("Due to steamAPI limitations this command has a small cooldown. Please try again shortly.");
+function lackingLevel(embed,level){
+	embed.setDescription(`You do not have the right level to use this!\n You must be level ${level}!`);
 	return embed;
 }
 
-function lackingLevel(level){
-	let embed = new Discord.MessageEmbed()
-		.setDescription(`You do not have the right level to use this!\n You must be level ${level}!`);
-	return embed;
-}
-
-function generalError(info){
-	let embed = new Discord.MessageEmbed()
-		.setDescription("Something has gone wrong, please notify Archie!");
+function generalError(embed,info){
+	embed.setDescription("Something has gone wrong, please notify Archie!");
 	console.log(info);
 	return embed;
 }
 
-function disabled(){
-	let embed = new Discord.MessageEmbed()
-		.setDescription("This command is currently disabled.");
+function somethingWentWrong(embed,info){
+	embed.setDescription("Something went wrong, please notify Archie.");
+	console.log(info);
 	return embed;
 }
 
-function somethingWentWrong(info){
-	let embed = new Discord.MessageEmbed()
-		.setDescription("Something went wrong, please notify Archie.");
-	console.log(info);
+function unknownError(embed,location){
+	embed.setDescription(`Unknown error at location\`${location}\``);
 	return embed;
 }
