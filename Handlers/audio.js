82cc7580c80f8a21caba7872f8d7f537e3499fc2
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-02-23 16:54:32 +0000

    Fixed audio handler

--- Handlers/audio.js
+++ Handlers/audio.js
@@ -4,17 +4,19 @@ const Discord = require("discord.js");
 var adjustableConfig;
 var isPlaying = false;
 var currentDispatcher = null;
+var bot;
 
 exports.isPlaying = isPlaying;
 
 exports.init = function init(){
 	adjustableConfig = require("./../oliverbot.js").adjustableConfig;
+	bot = require("./../oliverbot.js").bot;
 }
 
-exports.handler = function handler(message,commands,args){
+exports.handler = function handler(message,command,args){
 	switch (command){
 		case "playaudio":
-			playAudio(message);
+			setupTune(message,args,args === null ? true : false);
 			break;
 		case "stopaudio":
 			stopAudio(message);
@@ -22,40 +24,50 @@ exports.handler = function handler(message,commands,args){
 	}
 }
 
-async function playAudio(message){
+async function setupTune(message,args,fromFile){
 	let voiceChannel = message.member.voice.channel;
 	if (!voiceChannel){
 		message.reply("You must be in a voice channel!");
-	}else if (!adjustableConfig.music.generalAudio){
-		message.channel.send(issueEmbed.grabEmbed(4,null));
+	//}else if (!adjustableConfig.music.generalAudio){
+	//	message.channel.send(issueEmbed.grabEmbed(4,null));
 	}else if (isPlaying){
 		message.reply("I'm already playing!");
 	}else{
-		song = args.join("");
-		if (song.includes("https://www.youtube.com/watch?v=")){
-			let songInfo = await ytdl.getInfo(song);
-			playAudio(message,args,voiceChannel);
-			let embed = new Discord.MessageEmbed()
-				.setTitle("Now Playing")
-				.setColor('#add8e6')
-				.addField(`Song Info:`,`${songInfo.title}\n${songInfo.video_url}\n${Math.floor(songInfo.length_seconds / 3600)}h ${Math.floor(((songInfo.length_seconds / 3600) - Math.floor(songInfo.length_seconds / 3600)) * 60)}m ${songInfo.length_seconds % 60}s\n${songInfo.player_response.videoDetails.author}`)
-				.setThumbnail(`${message.author.displayAvatarURL()}`)
-				.setTimestamp();
-			message.channel.send(embed).then(msg => {
-				setTimeout(function(){
-					msg.delete();
-				},songInfo.length_seconds * 1000);
-			});
+		if (fromFile){
+			let file = fs.readFileSync("./datafile.json").toString();
+			file = JSON.parse(file);
+			let a = glob.getRandomInt(file.pingedSounds.length);
+			let randomsong = file.pingedSounds[a].toString();
+			playAudio(message,randomsong,voiceChannel);
 		}else{
-			message.reply("Please enter a valid youtube link!");
+			song = args.join("");
+			if (song.includes("https://www.youtube.com/watch?v=")){
+				let songInfo = await ytdl.getInfo(song);
+				playAudio(message,song,voiceChannel);
+				console.log(songInfo);
+				let embed = new Discord.MessageEmbed()
+					.setTitle("Now Playing")
+					.setColor('#add8e6')
+					.addField(`Song Info:`,`${songInfo.title}\n${songInfo.video_url}\n${Math.floor(songInfo.length_seconds / 3600)}h ${Math.floor(((songInfo.length_seconds / 3600) - Math.floor(songInfo.length_seconds / 3600)) * 60)}m ${songInfo.length_seconds % 60}s\n${songInfo.player_response.videoDetails.author}`)
+					.setThumbnail(`${message.author.displayAvatarURL()}`)
+					.setTimestamp();
+				message.channel.send(embed).then(msg => {
+					setTimeout(function(){
+						msg.delete();
+					},songInfo.length_seconds * 1000);
+				});
+			}else{
+				message.reply("Please enter a valid youtube link!");
+			}
 		}
 	}
 }
 
 function stopAudio(message){
-	if (!adjustableConfig.music.generalAudio){
-		message.channel.send(issueEmbed.grabEmbed(4,null));
-	}else if (!isPlaying){
+	//if (!adjustableConfig.music.generalAudio){
+	//	message.channel.send(issueEmbed.grabEmbed(4,null));
+	//}else
+	if (!isPlaying){
 		message.channel.send("I am not currently in a voice channel!");
 	}else if (!currentDispatcher){
 		message.channel.send("I am not currently playing anything!");
@@ -72,21 +84,24 @@ function stopAudio(message){
 	}
 }
 
-async function playAudio(message,args,voiceChannel){
+async function playAudio(message,song,voiceChannel){
 	isPlaying = true;
 	voiceChannel.join().then(connection =>{
+		connection.voice.setSelfDeaf(true);
 		currentDispatcher = connection
 			.play(
-        	  	ytdl(args.join(""),{filter:'audioonly',quality:'highestaudio',highWaterMark:1<<25}, {highWaterMark: 1},{bitrate: 192000})
+        	  	ytdl(song,{filter:'audioonly',quality:'highestaudio',highWaterMark:1<<25}, {highWaterMark: 1},{bitrate: 192000})
       		)
       		.on("finish",() =>{
       			voiceChannel.leave();
       			isPlaying = false;
+      			currentDispatcher.destroy();
       		})
       		.on("error",e=>{
       			console.error(e);
      		 	voiceChannel.leave();
      		 	isPlaying = false;
+     		 	currentDispatcher.destroy();
      		});
      	currentDispatcher.setVolumeLogarithmic(1);
     });
