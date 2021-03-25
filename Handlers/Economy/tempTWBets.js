07dd635ad7bfab0c5b2dfc9555f32a4658f9114e
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-03-25 01:16:40 +0000

    Updated for setup

--- Handlers/Economy/tempTWBets.js
+++ Handlers/Economy/tempTWBets.js
@@ -16,30 +16,34 @@ exports.handler = function handler(message,args){
 }
 
 function addUpdate(message,args){
-	let validOptions = ["af","cc","cf","crss","gt","se","uc"];
+	let validOptions = ["af","cc","cf","crss","gt","se","uc","goi"];
 	if (new Date().getTime() <= 1616259600000){
 		if (Array.isArray(args) && args.length === 2){
 			let amount = parseInt(args[0]);
 			if (!isNaN(amount) && amount > 0){
-				if (validOptions.indexOf(args[1].toLowerCase()) !== -1){
-					db.mainDatabaseConnectionPool.query(`SELECT * FROM shittyTempBets WHERE user=${message.author.id}`, (err,rows) => {
-						if (rows.length < 1){
-							db.mainDatabaseConnectionPool.query(`INSERT INTO shittyTempBets values ('${message.author.id}',${amount},'${args[1]}')`);
-							message.channel.send(`Your bet has been added.\n\`${args[1].toUpperCase()}\` : \`${amount}\``);
-						}else if (rows.length > 1){
-							message.channel.send("It seems you are duplicated, contact Archie.");
-						}else{
-							if (rows[0].amount < amount){
-								db.mainDatabaseConnectionPool.query(`UPDATE shittyTempBets SET amount=${amount}, team='${args[1]}' WHERE user='${message.author.id}'`);
-								message.channel.send(`Your bet has been updated.\n\`${rows[0].team}\` : \`${amount}\``);
+				db.mainDatabaseConnectionPool.query(`SELECT giraffeCoins from inventoryGT where ID='${message.author.id}'`, (err,rows) => {
+					if (amount > parseFloat(rows[0].giraffeCoins).toFixed(2)){
+						message.channel.send("You cannot bet more than you have!");
+					}else if (validOptions.indexOf(args[1].toLowerCase()) !== -1){
+						db.mainDatabaseConnectionPool.query(`SELECT * FROM shittyTempBets WHERE user=${message.author.id}`, (err,rows) => {
+							if (rows.length < 1){
+								db.mainDatabaseConnectionPool.query(`INSERT INTO shittyTempBets values ('${message.author.id}',${amount},'${args[1]}')`);
+								message.channel.send(`Your bet has been added.\n\`${args[1].toUpperCase()}\` : \`${amount}\``);
+							}else if (rows.length > 1){
+								message.channel.send("It seems you are duplicated, contact Archie.");
 							}else{
-								message.channel.send("You cannot place a lower bet than you already had placed!");
+								if (rows[0].amount <= amount){
+									db.mainDatabaseConnectionPool.query(`UPDATE shittyTempBets SET amount=${amount}, team='${args[1]}' WHERE user='${message.author.id}'`);
+									message.channel.send(`Your bet has been updated.\n\`${args[1].toUpperCase()}\` : \`${amount}\``);
+								}else{
+									message.channel.send("You cannot place a lower bet than you already had placed!");
+								}
 							}
-						}
-					});
-				}else{
-					message.channel.send("Please enter a valid team!\n`AF,CC,CF,CRSS,GT,SE,UC`");
-				}
+						});
+					}else{
+						message.channel.send("Please enter a valid team!\n`AF,CC,CF,CRSS,GOI,GT,SE,UC`");
+					}
+				});
 			}else{
 				message.channel.send("Please enter a valid amount!");
 			}
