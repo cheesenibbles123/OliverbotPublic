7e3451d1da2bfd5ce1da88b81e2f13ff79b169b7
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-04-13 18:09:29 +0100

    Fixed argument references

--- commands/today.js
+++ commands/today.js
@@ -11,13 +11,13 @@ module.exports = {
 			if (allowedInputs.indexOf(args[0]) >= 0){
 				let date = new Date();
 				fetch(`http://history.muffinlabs.com/date/${parseInt(date.getMonth()) + 1}/${date.getDate()}`).then(res => res.json()).then(response => {
-					if (argument === "events"){
+					if (args[0] === "events"){
 				 		let num = glob.getRandomInt(response.data.Events.length);
 						message.channel.send("```"+`${response.data.Events[num].year} - ${response.data.Events[num].text}`+"```");
-					}else if (argument === "births"){
+					}else if (args[0] === "births"){
 						let num = glob.getRandomInt(response.data.Births.length);
 						message.channel.send("```"+`${response.data.Births[num].year} - ${response.data.Births[num].text}`+"```");
-					}else if (argument === "deaths"){
+					}else if (args[0] === "deaths"){
 						let num = glob.getRandomInt(response.data.Deaths.length);
 						message.channel.send("```"+`${response.data.Deaths[num].year} - ${response.data.Deaths[num].text}`+"```");
 					}
