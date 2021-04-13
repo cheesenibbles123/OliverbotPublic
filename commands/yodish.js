9cceeb044a23507a1439fcdae4489edba564aa6c
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-04-13 17:02:34 +0100

    Converted to async

--- commands/yodish.js
+++ commands/yodish.js
@@ -4,7 +4,7 @@ module.exports = {
 	name: "yodish",
 	args: [0,100],
 	help: "Converts the input text to yodish.",
-	execute: (message,args) => {
+	execute: async (message,args) => {
 		let text = args.join(" ");
 		if (text.toLowerCase().includes("everyone") || text.toLowerCase().includes("here")){
 			message.channel.send("Nah.");
