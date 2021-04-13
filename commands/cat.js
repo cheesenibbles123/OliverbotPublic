15a176c6a2923beaf1ed68009e4e2d4a59835a26
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-04-13 18:08:54 +0100

    Fixed type issue

--- commands/cat.js
+++ commands/cat.js
@@ -5,7 +5,7 @@ module.exports = {
 	args: 0,
 	help: "Gets a random cat image",
 	execute: (message,args) => {
-		fetch("https://api.thecatapi.com/v1/images/search").then(res => res.text()).then(response =>{
+		fetch("https://api.thecatapi.com/v1/images/search").then(res => res.json()).then(response =>{
 			message.channel.send(response[0].url);
 		});
 	}
