5c7765395cbdc0c893ee6e497fcfde41474dc13f
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-04-13 18:08:16 +0100

    Temporarily disabled alternion

--- commands/Owner/updateAlternion.js
+++ commands/Owner/updateAlternion.js
@@ -1,11 +1,11 @@
-const alt = require("./alternion");
+//const alt = require("./alternion");
 
 module.exports = {
-	name: "do",
+	name: "updatealternion",
 	args: [1,200],
 	help: "Runs provided text as code",
 	users: ["337541914687569920"],
 	execute: async (message,args) => {
-		alt.globalJsonUpdate();
+		//alt.globalJsonUpdate();
 	}
 }
