a36a31a2e5b9315ea76f10ca6026d1374e45ae97
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-04-21 22:47:59 +0100

    Fixed registration system

--- commands/Alternion/_register.js
+++ commands/Alternion/_register.js
@@ -1,78 +1,55 @@
 const db = require("./../_databaseSetup.js");
 const shared = require("./_sharedFunctions.js");
+const Discord = require("discord.js");
 
 module.exports = {
 	name: "register",
 	args: 3,
-	help: "Allows a user to be registered into the database",
+	help: "Allows a user to be registered into the database\n;Alternion Register `{steamid}` `{discordid}`",
 	users: ["337541914687569920"],
 	execute: (message,args) => {
 
-		message.channel.send("Not currently active.");
+		if (!isNaN(parseInt(args[1]))){
+			if (!args[2] || !isNaN(parseInt(args[2]))){
+				if (!args[2]){
+					args[2] = null;
+				}
+				handleAlternionRegistration(message,args[1],args[2]);
+			}
+		}
+
 	}
 }
 
-function handleAlternionRegistration(message,term,steamID,discordID){
-	let registrationEmbed = new Discord.MessageEmbed().setTitle("Registration");
+function handleAlternionRegistration(message,steamID,discordID){
+	let registrationEmbed = new Discord.MessageEmbed()
+		.setTitle("Registration");
+	
 	let list = "";
-	switch (term){
-		case "steam":
-			db.alternionConnectionPool.query(`SELECT * FROM User WHERE Steam_ID="${steamID}"`, (err,rows) => {
 
-				if (rows.length > 1){
+	db.alternionConnectionPool.query(`SELECT ID,Steam_ID,Discord_ID FROM User WHERE Steam_ID='${steamID}'`, (err,rows) => {
 
-					for (let i = 0; i < rows.length; i++){
-						list += `${rows[i].ID}: ${rows[i].steam_id} - ${rows[i].discord_id}\n`;
-					}
+		if (rows.length > 1){
 
-					registrationEmbed.addField("Heres a list of users with that steamID", list);
-					sendAlternionEmbed(message,registrationEmbed,false);
+			for (let i = 0; i < rows.length; i++){
+				list += `${rows[i].ID}: ${rows[i].Steam_ID} - <@${rows[i].Discord_ID}>\n`;
+			}
 
-				}else if (rows.length < 1){
-					// Create user
-					db.alternionConnectionPool.query(`SELECT Count(*) FROM User`, (err,countRows) => {
-						db.alternionConnectionPool.query(`INSERT INTO User (ID,Steam_ID,Discord_ID) VALUES (${countRows.count}, ${steamID}, ${discordID})`);
-						registrationEmbed.setDescription(`Added user into the database!\n\`${countRows.count}\`, \`${steamID}\`, \`${discordID}\``);
-						sendAlternionEmbed(message,registrationEmbed,false);
-					});
-				}else if (rows.length === 1){
-					// Already exists, output ID and discord_id
-					registrationEmbed.setDescription(`User already exists!\n\`${rows[0].ID}\`, \`${rows[0].steam_id}\`, \`${rows[0].discord_id}\``);
-					sendAlternionEmbed(message,registrationEmbed,false);
-				}
+			registrationEmbed.addField("Heres a list of users with that steamID", list);
+			message.channel.send(registrationEmbed);
 
+		}else if (rows.length < 1){
+			// Create user
+			db.alternionConnectionPool.query(`SELECT Count(*) as count FROM User`, (err,countRows) => {
+				db.alternionConnectionPool.query(`INSERT INTO User (ID,Steam_ID,Discord_ID) VALUES (${countRows[0].count}, ${steamID}, ${discordID})`);
+				registrationEmbed.setDescription(`Added user into the database!\n\`${countRows[0].count}\`, \`${steamID}\`, \`${discordID}\``);
+				message.channel.send(registrationEmbed);
 			});
-			break;
-		case "discord":
-			db.alternionConnectionPool.query(`SELECT * FROM User WHERE Discord_ID="${steamID}"`, (err,rows) => {
+		}else if (rows.length === 1){
+			// Already exists, output ID and discord_id
+			registrationEmbed.setDescription(`User already exists!\n\`${rows[0].ID}\`, \`${rows[0].Steam_ID}\`, \`${rows[0].Discord_ID}\``);
+			message.channel.send(registrationEmbed);
+		}
 
-				if (rows.length > 1){
-
-					for (let i = 0; i < rows.length; i++){
-						list += `${rows[i].ID}: ${rows[i].steam_id} - ${rows[i].discord_id}\n`;
-					}
-
-					registrationEmbed.addField("Heres a list of users with that discordID", list);
-					sendAlternionEmbed(message,registrationEmbed,false);
-
-				}else if (rows.length < 1){
-					// Create user
-					db.alternionConnectionPool.query(`SELECT Count(*) FROM User`, (err,countRows) => {
-						db.alternionConnectionPool.query(`INSERT INTO User (ID,Steam_ID,Discord_ID) VALUES (${countRows.count}, ${discordID}, ${steamID})`);
-						registrationEmbed.setDescription(`Added user into the database!\n\`${countRows.count}\`, \`${discordID}\`, \`${steamID}\``);
-						sendAlternionEmbed(message,registrationEmbed,false);
-					});
-				}else if (rows.length === 1){
-					// Already exists, output ID and discord_id
-					registrationEmbed.setDescription(`User already exists!\n\`${rows[0].ID}\`, \`${rows[0].steam_id}\`, \`${rows[0].discord_id}\``);
-					sendAlternionEmbed(message,registrationEmbed,false);
-				}
-
-			});
-			break;
-		default:
-			registrationEmbed.setDescription("Please check you entered a valid feature\n`discord` `steam`");
-			sendAlternionEmbed(message,registrationEmbed,false);
-			break;
-	}
+	});
 }
