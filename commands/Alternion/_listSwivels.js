a2678ace35d67d540a68af9f1fd2a3894e4a2edc
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-05-07 12:06:22 +0100

    Added listSwivels

---
+++ commands/Alternion/_listSwivels.js
@@ -0,0 +1,37 @@
+const db = require("./../_databaseSetup.js");
+const Discord = require("discord.js");
+const shared = require("./_sharedFunctions.js");
+
+module.exports = {
+	name: "listcannons",
+	args: 2,
+	execute: (message, args) => {
+
+		let id = message.author.id;
+		let pubPriv = true;
+		if (args[1]){
+			pubPriv = (args[1].toLowerCase() !== "public");
+		}
+		let embed = new Discord.MessageEmbed()
+			.setTitle("Available Swivel Skins - ")
+			.setFooter("The formatting is: - `swivel_id` : Swivel Name -");
+
+		if (pubPriv){
+			db.alternionConnectionPool.query(`SELECT Team_ID from User where discord_id='${message.author.id}'`, (err,rows1) => {
+				db.alternionConnectionPool.query(`(SELECT Swivel.Name, Swivel.Display_Name FROM LimitedSwivels INNER JOIN User ON User_ID = User.ID INNER JOIN Swivel ON Allowed_Swivel_ID = Swivel.ID WHERE User.Discord_ID='${message.author.id}') UNION (SELECT Name,Display_Name FROM Swivel WHERE Team_ID=${rows1[0].Team_ID} AND IF ( ${rows1[0].Team_ID} != 0, 1, 0) = 1 )`, (err,rows) => {
+					
+					embed.setDescription(shared.iterateOver(rows,"Cannons"));
+					message.channel.send(embed);
+
+				});
+			});
+		}else{
+			db.alternionConnectionPool.query(`SELECT Name, Display_Name FROM Swivel WHERE Limited!=1`, (err,rows) => {
+				
+				embed.setDescription(shared.iterateOver(rows,"Cannons"));
+				message.channel.send(embed);
+
+			});
+		}
+	}
+}
