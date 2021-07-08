b857c700bac20a445eaafdf019e2def4c0f7ede3
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-07-08 18:22:41 +0100

    Added ship status function

---
+++ commands/WIP/_sharedFunctions.js
@@ -0,0 +1,14 @@
+module.exports = {
+	getShipStatus: (status) => {
+		switch (status.id){
+			case 0:
+				return "idle";
+			case 1:
+				return `travelling until ${new Date(staus.FinishAction)}`;
+			case 2:
+				return `unloading/loading cargo until ${new Date(staus.FinishAction)}`;
+			default:
+				return null;
+		}
+	}
+}
