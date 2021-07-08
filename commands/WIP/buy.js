7aae63b826dbc561aed3ad08e5f517f7a6f0ab9e
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-07-08 18:21:38 +0100

    updated to allow purchasing of items

--- commands/WIP/buy.js
+++ commands/WIP/buy.js
@@ -26,15 +26,25 @@ module.exports = {
 		dbSetup.query(`SELECT * FROM Player WHERE ID='${message.author.id}'`, (err,player) => {
 			if (player.length > 1 || player.length === 0){
 				message.channel.send("Error fetching player data.");
-				console.log(player);
 			}else{
 				dbSetup.query(`SELECT * FROM Port WHERE ID=${player[0].Location};SELECT count(*) AS count FROM Player WHERE Location=${player[0].Location}`, (err,portInfo) => {
-					console.log(portInfo);
 					let port = portInfo[0][0];
-					console.log(port);
-					console.log(port.Items);
+
 					if (port.Items.includes(args[0])){
+
 						message.channel.send(`Found item \`${args[0]}\`at port ${port.Name}!\nCurrently ${portInfo[1][0].count} players are docked there.`);
+						let inv = JSON.parse(player[0].Inventory);
+
+						dbSetup.query(`SELECT DefaultLayout.Info FROM DefaultLayout INNER JOIN Ship on Ship.ID=DefaultLayout.ID WHERE Ship.Type='${args[0]}'`, (err,rows) => {
+							console.log(rows);
+							console.log(inv);
+							inv.push(JSON.parse(rows[0].Info));
+							console.log(inv);
+							let sql = `UPDATE Player SET Inventory='${JSON.stringify(inv)}' WHERE ID='${message.author.id}'`;
+							console.log(sql);
+							dbSetup.query(sql);
+						});
+
 					}else{
 						message.channel.send(`Item \`${args[0]}\` not found at port ${port.Name}.\nCurrently ${portInfo[1][0].count} players are docked there.`);
 					}
