5ce9b76e3c1ef1aef8930adc64ff9f9695838920
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-07-08 01:58:55 +0100

    Added args key

--- commands/Owner/updateAlternion.js
+++ commands/Owner/updateAlternion.js
@@ -3,6 +3,7 @@ const alt = require("./../Alternion/_sharedFunctions.js");
 module.exports = {
 	name: "updatealternion",
 	help: "Force Runs Alternion update for all users",
+	args: 0,
 	users: ["337541914687569920"],
 	category: "Owner",
 	execute: (message,args) => {
