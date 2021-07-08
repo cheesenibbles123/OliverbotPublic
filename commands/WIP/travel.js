799200d4c3844e9e36bd4b2532dd928750cbc45c
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-07-08 18:22:11 +0100

    Added arrival calculation, as well as basic info

--- commands/WIP/travel.js
+++ commands/WIP/travel.js
@@ -14,6 +14,7 @@ let bot;
 
 module.exports = {
 	name: "travel",
+	args: 2,
 	help: "Search for an item by its ID",
 	users: ["337541914687569920"],
 	category: "Economy2",
@@ -21,6 +22,9 @@ module.exports = {
 		bot = botInstance;
 	},
 	execute: (message,args) => {
+		let ship = args[0];
+		let port = args.shift().join(" ").toLowerCase();
+		
 		dbSetup.query(`SELECT * FROM Player WHERE ID='${message.author.id}'`, (err,player) => {
 			if (player.length > 1 || player.length === 0){
 				message.channel.send("Error fetching player data.");
@@ -31,7 +35,7 @@ module.exports = {
 					let port2 = null;
 
 					for (let i=0; i < ports.length; i++){
-						if (ports[i].Name.toLowerCase() === args.join(" ")){
+						if (ports[i].Name.toLowerCase() === port){
 							port2 = ports[i];
 						}else if (ports[i].ID === player[0].Location){
 							port1 = ports[i];
@@ -54,10 +58,12 @@ module.exports = {
 
 						message.channel.send(`Distance to travel: ${C}KM\nCosting ${goods} goods.`);
 
-						// Usage of 1 Consumer Goods per KM
+						let speed = 9.26;
+
+						// Usage of 1 Consumer Goods per 74K
 						if (inv.goods >= Math.ceil(C)){
 							message.channel.send(`Setting sail for ${port2.Name}`);
-							let sql =`UPDATE Player SET Location=${port2.ID} WHERE ID='${message.author.id}'`;
+							let sql =`UPDATE Player SET Location=${port2.ID}, LastDeparture=${new Date().getTime()}, ArrivalTime=${Math.floor(new Date().getTime() + ((C/speed) * 3600000))} WHERE ID='${message.author.id}'`;
 							console.log(sql);
 							dbSetup.query(sql);
 						}else{
