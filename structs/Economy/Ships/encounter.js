d8f8853a0c18d37407ee31ea2d5814b03700e989
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-07-12 02:02:38 +0100

    Setup initial random ship generation

--- structs/Economy/Ships/encounter.js
+++ structs/Economy/Ships/encounter.js
@@ -1,22 +1,49 @@
 const shared = require('./../../shared.js');
+const ship = require('./ship.js');
 
 class EncounterShip {
 	constructor(data){
 		this.spotter = data.spotter; // Boolean
 		this.shipID = data.shipID; // Int
-		this.ship = data.ship; // Object
+		if (data.ship){
+			this.ship = data.ship; // Object
+		}else{
+			this.ship = null;
+		}
 	}
-	generateRandom(difficultyLevel){
-		economy.query(`SELECT * FROM Upgrades WHERE Rarity<=${difficultyLevel}`, (err,upgrades) => {
-			let affectedStats = [];
+	async generateRandom(difficultyLevel){
+		return new Promise((resolve,reject) => {
+			const genShip = new Promise((resolve,reject) => {
+				economy.query(`SELECT * FROM DefaultLayouts where ID=${this.shipID}`, (err,rows) => {
+					this.ship = new ship({
+						rows[0].shipInfo
+					});
+					resolve();
+				});
+			});
 
-			for (let i=0; i<upgrades.length; i++){
-				if (shared.getRandomInt(5) === 1 && !affectedStats.includes(upgrades[i].AffectsStat)){
-					affectedStats.push(upgrades[i].AffectsStat);
-					this.ship.upgrades.push(upgrades[i]);
-				}
+			const getUpgrades = new Promise((resolve,reject) => {
+				economy.query(`SELECT * FROM Upgrades WHERE Rarity<=${difficultyLevel}`, (err,upgrades) => {
+					let affectedStats = [];
+					for (let i=0; i<upgrades.length; i++){
+						if (shared.getRandomInt(5) === 1 && !affectedStats.includes(upgrades[i].AffectsStat)){
+							affectedStats.push(upgrades[i].AffectsStat);
+							this.ship.upgrades.push(upgrades[i]);
+						}
+					}
+					resolve();
+				});
+			});
+
+			if (this.ship === null){
+				await genShip();
 			}
 
+			await getUpgrades();
+			
+			resolve();
 		});
 	}
 }
+
+exports.EncounterShip = EncounterShip;
