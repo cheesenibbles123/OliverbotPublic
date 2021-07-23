c24d75b23229947f7e41d73f9946cca64a973cb5
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-07-23 21:49:32 +0100

    Converted SQL to uppercase

--- commands/Admin/deleteRole.js
+++ commands/Admin/deleteRole.js
@@ -9,10 +9,10 @@ module.exports = {
 	execute: (message,args) => {
 		if (args[0].indexOf(":") !== -1){
 			//db.deleteReactionRole(message,args[0].split(":")[1]);
-			db.configurationDatabaseConnectionPool.query(`delete from reactionRoles where emojiName='${args[0].split(":")[1]}'`);
+			db.configurationDatabaseConnectionPool.query(`DELETE FROM reactionRoles WHERE emojiName='${args[0].split(":")[1]}'`);
 		}else{
 			//db.deleteReactionRole(message,args[0]);
-			db.configurationDatabaseConnectionPool.query(`delete from reactionRoles where emojiName='${args[0]}'`);
+			db.configurationDatabaseConnectionPool.query(`DELETE FROM reactionRoles WHERE emojiName='${args[0]}'`);
 		}
 		db.loadReactionRolesFromDB();
 		message.channel.send("Role removed!");
