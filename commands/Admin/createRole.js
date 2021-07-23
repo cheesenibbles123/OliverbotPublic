741664f764f3e6cdd55d431511c5aa7404e0bbcd
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-07-23 21:49:11 +0100

    Converted SQL to uppercase

--- commands/Admin/createRole.js
+++ commands/Admin/createRole.js
@@ -10,9 +10,9 @@ module.exports = {
 		if (args[0].indexOf(":") !== -1){
 			let emojiID = args[0].split(":")[2].toString();
 			emojiID = emojiID.slice(0, emojiID.length - 1);
-			db.configurationDatabaseConnectionPool.query(`insert into reactionRoles values ('${args[0].split(":")[1]}' , '${emojiID}' , 'NA' , '${args[1].slice(3,args[1].length - 1)}' )`);
+			db.configurationDatabaseConnectionPool.query(`INSERT INTO reactionRoles VALUES ('${args[0].split(":")[1]}' , '${emojiID}' , 'NA' , '${args[1].slice(3,args[1].length - 1)}' )`);
 		}else{
-			db.configurationDatabaseConnectionPool.query(`insert into reactionRoles values ('${args[0]}' , 'NA' , 'unicode' , '${args[1].slice(3,args[1].length - 1)}' )`);
+			db.configurationDatabaseConnectionPool.query(`INSERT INTO reactionRoles VALUES ('${args[0]}' , 'NA' , 'unicode' , '${args[1].slice(3,args[1].length - 1)}' )`);
 		}
 		db.loadReactionRolesFromDB();
 		message.channel.send("Role added!");
