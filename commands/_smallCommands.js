57841e3754eba616cb0bc12a1cdb6b0de9a6679e
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-07-23 22:02:50 +0100

    Converted memegen to custom command

--- commands/_smallCommands.js
+++ commands/_smallCommands.js
@@ -10,37 +10,6 @@ exports.init = function init(){
 	adjustableConfig = require("./databaseSetup.js").adjustableConfig;
 }
 
-function memegen(message,args){
-	if (db.levelchecker(message,4)){
-		TrackingCommand = true;
-		if (adjustableConfig.apis.memegen){
-			let contents = message.content.split(",");
-	    	for (i=0;i<contents.length;){
-	        	let word = contents[i];
-	        	if (i === 0){
-	        	    word = word.slice(command.length+2);
-	        	}
-	        	word = word.replace(/_/g, "__");
-	        	word = word.replace(/-/g, "--");
-	   	    	word = word.replace(/ /g, "-");
-	   	    	word = word.replace(/\?/g, "~q");
-	   	     	word = word.replace(/%/g, "~p");
-	   	     	word = word.replace(/#/g, "~h");
-	   	     	word = word.replace(/''/g, `"`);
-	    	    word = word.replace(/\//g, "~s");
-	    	    contents[i] = word;
-	    	    i++;
-	    	}
-			let URL = `https://memegen.link/${contents[0]}/${contents[1]}/${contents[2]}.jpg`;
-			fetch(URL).then(response =>{
-			    message.channel.send(response.url);
-			});
-		}
-	}else{
-		issueEmbed.grabEmbed(2,4);
-	}
-}
-
 function insultUser(message,args){
 	if (levelchecker(message,7)){
 		let fine = true;
