72d75e6c2ac71c21a9b00eeced302ef167455cb1
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-07-23 22:01:00 +0100

    Moved wordsAPI to custom command

--- commands/_smallCommands.js
+++ commands/_smallCommands.js
@@ -10,31 +10,6 @@ exports.init = function init(){
 	adjustableConfig = require("./databaseSetup.js").adjustableConfig;
 }
 
-function wordsAPI(message,args){
-	if (!adjustableConfig.apis.dictionary){
-		message.channel.send(issueEmbed.grabEmbed(4,null));
-	}else
-	if (args.length > 1){
-		message.channel.send("The API only works with one word at a time :(");
-	}else
-	if (args.length === 0){
-		message.channel.send("Please enter a word to look up.");
-	}else{
-		fetch(`https://wordsapiv1.p.mashape.com/words/${args[0]}/definitions`).then(resp => resp.json()).then(response => {
-			let definitions = "";
-			for(let i = 0; i < response.definitions.length-1; i++) {
-			    definitions = definitions +`${response.definitions[i].definition} - ${response.definitions[i].partOfSpeech}\n"`;
-			}
-			let wordsAPIEmbed = new Discord.MessageEmbed()
-				.addTitle(`${args[0]}`)
-				.setDescription(`${definitions}`)
-				.setThumbnail("https://www.programmableweb.com/sites/default/files/styles/facebook_scale_width_200/public/WordsAPI%20Logo2.png?itok=vfPp_WC1")
-				.setTimestamp();
-			message.channel.send(wordsAPIEmbed);
-		});
-	}
-}
-
 function memegen(message,args){
 	if (db.levelchecker(message,4)){
 		TrackingCommand = true;
---
+++ commands/wordsApi.js
@@ -0,0 +1,29 @@
+const fetch = require("node-fetch");
+const Discord = require("discord.js");
+
+module.exports = {
+	name: "words",
+	args: [0,100],
+	help: "Looks up the meaning behind a given word",
+	execute: async (message,args) => {
+		if (args.length > 1){
+			message.channel.send("The API only works with one word at a time :(");
+		}else
+		if (args.length === 0){
+			message.channel.send("Please enter a word to look up.");
+		}else{
+			fetch(`https://wordsapiv1.p.mashape.com/words/${args[0]}/definitions`).then(resp => resp.json()).then(response => {
+				let definitions = "";
+				for(let i = 0; i < response.definitions.length-1; i++) {
+				    definitions = definitions +`${response.definitions[i].definition} - ${response.definitions[i].partOfSpeech}\n"`;
+				}
+				let wordsAPIEmbed = new Discord.MessageEmbed()
+					.addTitle(`${args[0]}`)
+					.setDescription(`${definitions}`)
+					.setThumbnail("https://www.programmableweb.com/sites/default/files/styles/facebook_scale_width_200/public/WordsAPI%20Logo2.png?itok=vfPp_WC1")
+					.setTimestamp();
+				message.channel.send(wordsAPIEmbed);
+			});
+		}
+	}
+}
