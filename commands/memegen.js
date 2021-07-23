7a03f5ee777b0004a62bdef9703fcafa52ebf933
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-07-23 22:05:08 +0100

    Removed level limit

--- commands/memegen.js
+++ commands/memegen.js
@@ -5,30 +5,26 @@ module.exports = {
 	args: [0,100],
 	help: "Generates a meme based off the input",
 	execute: async (message,args) => {
-		if (db.levelchecker(message,4)){
-			let contents = message.content.split(",");
-	    	for (i=0;i<contents.length;){
-	        	let word = contents[i];
-	        	if (i === 0){
-	        	    word = word.slice(command.length+2);
-	        	}
-	        	word = word.replace(/_/g, "__");
-	        	word = word.replace(/-/g, "--");
-	   	    	word = word.replace(/ /g, "-");
-	   	    	word = word.replace(/\?/g, "~q");
-	   	     	word = word.replace(/%/g, "~p");
-	   	     	word = word.replace(/#/g, "~h");
-	   	     	word = word.replace(/''/g, `"`);
-	    	    word = word.replace(/\//g, "~s");
-	    	    contents[i] = word;
-	    	    i++;
-	    	}
-			let URL = `https://memegen.link/${contents[0]}/${contents[1]}/${contents[2]}.jpg`;
-			fetch(URL).then(response =>{
-			    message.channel.send(response.url);
-			});
-		}else{
-			issueEmbed.grabEmbed(2,4);
-		}
+		let contents = message.content.split(",");
+    	for (i=0;i<contents.length;){
+        	let word = contents[i];
+        	if (i === 0){
+        	    word = word.slice(command.length+2);
+        	}
+        	word = word.replace(/_/g, "__");
+        	word = word.replace(/-/g, "--");
+   	    	word = word.replace(/ /g, "-");
+   	    	word = word.replace(/\?/g, "~q");
+   	     	word = word.replace(/%/g, "~p");
+   	     	word = word.replace(/#/g, "~h");
+   	     	word = word.replace(/''/g, `"`);
+    	    word = word.replace(/\//g, "~s");
+    	    contents[i] = word;
+    	    i++;
+    	}
+		let URL = `https://memegen.link/${contents[0]}/${contents[1]}/${contents[2]}.jpg`;
+		fetch(URL).then(response =>{
+		    message.channel.send(response.url);
+		});
 	}
 }
