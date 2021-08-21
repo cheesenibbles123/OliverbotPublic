857d070075296771c75cd4a742c9ac9c6f2251f6
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-08-21 03:02:55 +0100

    Setup initial v13 conversion

--- commands/quizQuestions.js
+++ commands/quizQuestions.js
@@ -1,6 +1,7 @@
 const glob = require("./_globalFunctions");
 const db = require("./../startup/database.js");
 const Discord = require("discord.js");
+const { reply } = require("./_globalFunctions.js");
 
 isNotLocked = true;
 
@@ -9,34 +10,61 @@ module.exports = {
 	args: [0,1],
 	help: "Displays a quiz question for you to awnser",
 	usage: "<income>",
+	options : [
+		{
+			name : "Income",
+			description : "Provides income up to 5GC for 1st place, has a cooldown.",
+			type : 3,
+			required : false,
+			choices : [
+				{
+					name : "Income",
+					value: "income"
+				}
+			]
+		}
+	],
 	execute: (message,args) => {
 		if (isNotLocked){
 			switch (args[0]){
 				case "income":
-					checkQuizAllowances(message,args);
+					checkQuizAllowances(message,args,true);
 					break;
 				default:
-					getRandomQuizQuestion(message,false);
+					getRandomQuizQuestion(message,false,true);
+					break;
+			}
+		}
+	},
+	executeInteraction: (interaction,args) => {
+		if (isNotLocked){
+			switch (args[0]){
+				case "income":
+					checkQuizAllowances(interaction,args,false);
+					break;
+				default:
+					getRandomQuizQuestion(interaction,false,false);
 					break;
 			}
 		}
 	}
 }
 
-function checkQuizAllowances(message,args){
-	db.mainDatabaseConnectionPool.query(`SELECT * FROM inventoryGT where ID='${message.author.id}'`, (err,rows,fields) => {
+function checkQuizAllowances(event,args,isMessage){
+	let id = isMessage ? event.author.id : event.member.user.id;
+	db.mainDatabaseConnectionPool.query(`SELECT * FROM inventoryGT where ID='${id}'`, (err,rows,fields) => {
 
 		db.mainDatabaseConnectionPool.query("SELECT * FROM cooldowns WHERE name='quiz'", (err,rows2,fields) => {
 
 			if (rows[0].lastQuiz === null){
 
-				getRandomQuizQuestion(message,true);
-				db.mainDatabaseConnectionPool.query(`UPDATE inventoryGT SET lastQuiz='${new Date().getTime()}' WHERE ID='${message.author.id}'`);
+				getRandomQuizQuestion(event,true,isMessage);
+				db.mainDatabaseConnectionPool.query(`UPDATE inventoryGT SET lastQuiz='${new Date().getTime()}' WHERE ID='${id}'`);
 
 			}else if ((parseInt(rows[0].lastQuiz) + rows2[0].duration) < (new Date().getTime())){
 
-				getRandomQuizQuestion(message,true);
-				db.mainDatabaseConnectionPool.query(`UPDATE inventoryGT SET lastQuiz='${new Date().getTime()}' WHERE ID='${message.author.id}'`);
+				getRandomQuizQuestion(event,true,isMessage);
+				db.mainDatabaseConnectionPool.query(`UPDATE inventoryGT SET lastQuiz='${new Date().getTime()}' WHERE ID='${id}'`);
 
 			}else{
 				message.channel.send(`Please wait, your income on this command is currently on a ${parseInt(rows2[0].duration / 1000)}sec cooldown.`);
@@ -47,20 +75,21 @@ function checkQuizAllowances(message,args){
 	});
 }
 
-function getRandomQuizQuestion(message,isGainingIncome){
+function getRandomQuizQuestion(event,isGainingIncome,isMessage){
 	db.mainDatabaseConnectionPool.query("SELECT * FROM quiz", (err,rows,fields) => {
 		let num = glob.getRandomInt(rows.length - 1);
 		if (rows[num].format === "text"){
-			textQuizQuestions(message,rows[num].question,rows[num].awnsers,rows[num].timeFactor,rows[num].worthFactor,rows[num].maxAttempts,isGainingIncome);
+			textQuizQuestions(event,rows[num].question,rows[num].awnsers,rows[num].timeFactor,rows[num].worthFactor,rows[num].maxAttempts,isGainingIncome,isMessage);
 		}
 	});
 }
 
-async function textQuizQuestions(message,question,awnsers,timeFactor,worthFactor,maxAttempts,isGainingIncome){
+async function textQuizQuestions(event,question,awnsers,timeFactor,worthFactor,maxAttempts,isGainingIncome,isMessage){
 	let baseIncome = 5;
 	isNotLocked = false;
+	let channel = isMessage ? event.channel : bot.channels.cache.get(event.channelId);
 
-	message.channel.send(question).then(() => {
+	channel.send(question).then(() => {
 
 		let list = [];
 		let attempts = {};
@@ -75,7 +104,7 @@ async function textQuizQuestions(message,question,awnsers,timeFactor,worthFactor
 			if (response.attachments.size > 0){
 				response.channel.send("Attachments are not supported as awnsers.");
 				return false;
-			}else if ( awnsers.indexOf(response.content.toLowerCase()) !== -1 && list.indexOf(response.author) === -1 && !response.author.bot && attempts[message.author.id] <= maxAttempts ){
+			}else if ( awnsers.indexOf(response.content.toLowerCase()) !== -1 && list.indexOf(response.author) === -1 && !response.author.bot && attempts[response.author.id] <= maxAttempts ){
 				return true;
 			}else{
 				if (attempts[response.author.id] > maxAttempts && attempts[response.author.id] < maxAttempts + 2 && !response.author.bot){
@@ -85,13 +114,13 @@ async function textQuizQuestions(message,question,awnsers,timeFactor,worthFactor
 			}
 		};
 
-		let collector = message.channel.createMessageCollector(filter, { time: 15000 * timeFactor });
+		let collector = channel.createMessageCollector(filter, { time: 15000 * timeFactor });
 
 		collector.on('collect', msg => {
 			if (list.indexOf(msg.author) === -1){
 				list.push(msg.author);
 				msg.delete();
-				message.channel.send(`${msg.author} got the correct awnser!`);
+				channel.send(`${msg.author} got the correct awnser!`);
 			}
 		});
 
@@ -117,9 +146,9 @@ async function textQuizQuestions(message,question,awnsers,timeFactor,worthFactor
 					}
 				}
 				embed.setDescription(allUsers);
-				message.channel.send(embed);
+				channel.send(embed);
 			}else{
-				message.channel.send('Sadly, right now, is not the moment we find out the answer to this question.');
+				channel.send('Sadly, right now, is not the moment we find out the answer to this question.');
 			}
 
 			isNotLocked = true;
