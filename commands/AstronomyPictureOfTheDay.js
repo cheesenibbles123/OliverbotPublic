55f595dcb3488041c31d32d9ba558b2db65d449f
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-08-21 03:32:15 +0100

    Added interaction support to apod

--- commands/AstronomyPictureOfTheDay.js
+++ commands/AstronomyPictureOfTheDay.js
@@ -1,30 +1,39 @@
 const fetch = require("node-fetch");
 const Discord = require("discord.js");
 const config = require("./../config.json");
+const {reply} = require("./_globalFunctions.js");
 
 module.exports ={
 	name: "apod",
 	args: 0,
 	help: "Displays today astronomy picture of the day!",
+	interactionSupport: true,
 	execute: (message,args) => {
-		fetch(`https://api.nasa.gov/planetary/apod?api_key=${config.apiKeys.nasa}`).then(res => res.json()).then(response => {
-			let explanation = response.explanation;
-			if (explanation.length > 1024){
-				explanation = explanation.slice(0,1021);
-				explanation = explanation + "...";
-			}
-			let aspdembed = new Discord.MessageEmbed()
-						.setTitle("Astronomy Picture of the Day")
-						.addField(`${response.title}`,`${explanation}`);
-			let linkURL = response.url.toString();
-			if (linkURL.substr(response.url.length -3) === "jpg" || linkURL.substr(response.url.length -3) === "png" || linkURL.substr(response.url.length -3) === "jpeg"){
-				aspdembed.setImage(`${response.url}`);
-			}else{
-				aspdembed.addField("URL:",`${response.url}`)
-			}
-			aspdembed.setFooter(`Date: ${response.date}`)	
-					.setTimestamp();
-			message.channel.send(aspdembed);
-		});
+		mainHandler(message,args,true);
+	},
+	executeInteraction: (interaction,args) => {
+		mainHandler(interaction,args,false);
 	}
+}
+
+function mainHandler(event,args,isMessage){
+	fetch(`https://api.nasa.gov/planetary/apod?api_key=${config.apiKeys.nasa}`).then(res => res.json()).then(response => {
+		let explanation = response.explanation;
+		if (explanation.length > 1024){
+			explanation = explanation.slice(0,1021);
+			explanation = explanation + "...";
+		}
+		let aspdembed = new Discord.MessageEmbed()
+					.setTitle("Astronomy Picture of the Day")
+					.addField(`${response.title}`,`${explanation}`);
+		let linkURL = response.url.toString();
+		if (linkURL.substr(response.url.length -3) === "jpg" || linkURL.substr(response.url.length -3) === "png" || linkURL.substr(response.url.length -3) === "jpeg"){
+			aspdembed.setImage(`${response.url}`);
+		}else{
+			aspdembed.addField("URL:",`${response.url}`)
+		}
+		aspdembed.setFooter(`Date: ${response.date}`)	
+				.setTimestamp();
+		reply(event,{embeds: [aspdembed]},isMessage);
+	});
 }
