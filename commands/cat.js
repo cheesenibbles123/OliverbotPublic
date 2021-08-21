d529935c2cdfa56e509750a293aead36bbfc0348
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-08-21 01:08:21 +0100

    Updated to work with slash commands

--- commands/cat.js
+++ commands/cat.js
@@ -3,10 +3,16 @@ const fetch = require("node-fetch");
 module.exports = {
 	name: "cat",
 	args: 0,
+	interactionSupport: true,
 	help: "Gets a random cat image",
 	execute: (message,args) => {
 		fetch("https://api.thecatapi.com/v1/images/search").then(res => res.json()).then(response =>{
 			message.channel.send(response[0].url);
+		});
+	},
+	executeInteraction: (interaction) => {
+		fetch("https://api.thecatapi.com/v1/images/search").then(res => res.json()).then(async response =>{
+			await interaction.editReply(response[0].url);
 		});
 	}
 }
