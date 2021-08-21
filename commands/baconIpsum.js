0dfba7542a2bc4ade5f8d1aaac0f7131e2c64340
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-08-21 03:34:11 +0100

    Added support for interactions

--- commands/baconIpsum.js
+++ commands/baconIpsum.js
@@ -7,20 +7,28 @@ module.exports = {
 	args: 0,
 	help: "Grabs a random extract from bacon ipsum",
 	category: "Meme",
+	interactionSupport: true,
 	execute: (message,args) => {
-		let opt = glob.getRandomInt(2);
-		let content = "";
-		if (opt === 0){
-			content += "https://baconipsum.com/api/?type=meat-and-filler";
-		}else
-		if (opt === 1){
-			content += "https://baconipsum.com/api/?type=meat-and-filler&start-with-lorem=1";
-		}
-		fetch(content).then(res => res.json()).then(response => {
-			let BaconEmbed = new Discord.MessageEmbed()
-				.setDescription(`${response[glob.getRandomInt(response.length)]}`)
-				.setTimestamp();
-			message.channel.send(BaconEmbed);
-		});
+		mainHandler(message,args,true);
+	},
+	executeInteraction: (interaction,args) => {
+		mainHandler(interaction,args,false);
 	}
+}
+
+function mainHandler(event,args,isMessage){
+	let opt = glob.getRandomInt(2);
+	let content = "";
+	if (opt === 0){
+		content += "https://baconipsum.com/api/?type=meat-and-filler";
+	}else
+	if (opt === 1){
+		content += "https://baconipsum.com/api/?type=meat-and-filler&start-with-lorem=1";
+	}
+	fetch(content).then(res => res.json()).then(response => {
+		let BaconEmbed = new Discord.MessageEmbed()
+			.setDescription(`${response[glob.getRandomInt(response.length)]}`)
+			.setTimestamp();
+		reply(event,{embeds: [BaconEmbed]},isMessage);
+	});
 }
