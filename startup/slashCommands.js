1901b5c0116c447240dec4f0fe5b2c205fdbe348
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-08-22 18:47:58 +0100

    Setup initial code for managing slash command permissions

--- startup/slashCommands.js
+++ startup/slashCommands.js
@@ -5,8 +5,7 @@ const config = require('./../config.json');
 module.exports = {
 	enabled: true,
 	init: async (bot) => {
-		console.log('Entered init');
-		let timer = ms => new Promise( res => setTimeout(res, ms));
+		const timer = ms => new Promise( res => setTimeout(res, ms));
 		let commandsNotFound = false;
 		while (commandsNotFound){
 			await timer(500);
@@ -15,17 +14,21 @@ module.exports = {
 			}
 		}
 
-		let rest = new REST({ version: '9' }).setToken(config.token);
+		const rest = new REST({ version: '9' }).setToken(config.token);
 
 		let slashCommands = [];
 		let addedCommands = [];
 
 		for (let commandName in bot.commands){
-			let command = bot.commands[commandName];
-			if (!command.roles && !command.users && (command.interactionSupport || typeof(command.executeGlobal) === "function") && addedCommands.indexOf(command.name) === -1){
+
+			if (/*!command.roles && !command.users &&*/ (command.interactionSupport || typeof(command.executeGlobal) === "function") && addedCommands.indexOf(command.name) === -1){
 				let data = {
 					name : command.name,
 					description : command['help'] ? command.help : (command['description'] ? command.description : "N/A"),
+				}
+
+				if (command['default_permission']){
+					command['default_permission'] = command.default_permission;
 				}
 
 				if (command.options){
@@ -35,12 +38,67 @@ module.exports = {
 				slashCommands.push(data);
 				addedCommands.push(command.name);
 			}
+
 		};
 
 		try{
+			// registering
 			await rest.put(
 				Routes.applicationGuildCommands(config.botID, config.serverInfo.serverId),
 				{body : slashCommands},
+			);
+		}catch(e){
+			console.error(e);
+		}
+
+		return; // todo : Need to learn more about permissions system for slash commands
+
+		const commands = await rest.get(
+			Routes.applicationGuildCommands(config.botID, config.serverInfo.serverId)
+		);
+
+		let permissionOverrides = [];
+
+		await commands.forEach( command => {
+			if (bot.commands[command.name].roles || bot.commands[command.name.users]){
+				let commandObject = {
+					id : command.id,
+					permissions : [],
+				}
+
+				/*if (bot.commands[command.name].roles){
+					bot.commands[command.name].roles.forEach(roleID => {
+						let obj = {
+							id : roleID,
+							type : 1,
+							permission : true
+						}
+						commandObject.permissions.push(obj);
+					});
+				}*/
+
+				if (bot.commands[command.name].users){
+					bot.commands[command.name].users.forEach(userID => {
+						let obj = {
+							id : userID,
+							type : 2,
+							permission : true
+						}
+						commandObject.permissions.push(obj);
+					});
+				}
+
+				permissionOverrides.push(commandObject);
+
+			}
+		});
+
+
+		try{
+			// Permissions
+			await rest.put(
+				`/applications/${config.botID}/guilds/${config.serverInfo.serverId}/commands/permissions`,
+				{body : permissionOverrides},
 			);
 		}catch(e){
 			console.error(e);
