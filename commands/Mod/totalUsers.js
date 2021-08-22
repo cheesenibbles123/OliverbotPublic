902d8b58d89798f44a07f72203acb94b86cc401f
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-08-22 15:34:20 +0100

    Setup command for slash support

--- commands/Mod/totalUsers.js
+++ commands/Mod/totalUsers.js
@@ -1,10 +1,18 @@
+const {reply} = require("./../_combinedResponses");
+let bot;
+
 module.exports = {
 	name: "totalusers",
-	args: 2,
 	help: "Displays total number of users",
 	roles: ["440514569849536512"],
+	users: ["337541914687569920"],
+	default_permission : false,
 	category: "Mod",
-	execute: (message,args) => {
-		message.channel.send(message.guild.members.cache.filter(member => !member.user.bot).size);
+	guildOnly: true,
+	init: (botInstance) => {
+		bot = botInstance;
+	},
+	executeGlobal: (event,args,isMessage) => {
+		reply(event,`${event.guild.members.cache.filter(member => !member.user.bot).size}`,isMessage);
 	}
 }
