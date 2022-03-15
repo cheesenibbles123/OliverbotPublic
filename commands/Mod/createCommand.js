3f3e039cf555b10f2823ec88a6fb7faadac4308c
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2022-03-15 11:19:07 +0000

    Added error logging on failed command create

--- commands/Mod/createCommand.js
+++ commands/Mod/createCommand.js
@@ -17,17 +17,19 @@ module.exports = {
 		if (bot.commands[args[0]]){
 			message.channel.send("That command already exists!");
 		}else{
-			db.configurationDatabaseConnectionPool.query(`INSERT INTO CustomCommands VALUES ('${args[0]}' , '${args.slice(1)}' )`);
-			let command = {
-				name: args[0],
-				help: "Custom Command",
-				category: "Custom",
-				execute: (message,args) => {
-					message.channel.send(args.slice(1).join(' '));
+			db.configurationDatabaseConnectionPool.query(`INSERT INTO CustomCommands VALUES ('${args[0]}' , '${args.slice(1)}' )`, (err) => {
+				if (err) return console.log(err);
+				let command = {
+					name: args[0],
+					help: "Custom Command",
+					category: "Custom",
+					execute: (message,content) => {
+						message.channel.send(args.slice(1).join(' '));
+					}
 				}
-			}
-			bot.commands[args[0]] = command;
-			message.reply("Your command is ready to go!");
+				bot.commands[args[0]] = command;
+				message.reply("Your command is ready to go!");
+			});
 		}
 	}
 }
